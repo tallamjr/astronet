e6178e3eae8a23829bc2e8e45a51f27470a6917f
Tarek Allam <t.allam.jr@gmail.com>
2020-09-30 01:42:45 +0100

    Test 'evaluate.py' can load saved model
    	modified:   evaluate.py
    	new file:   models/model-1601426331-bbb8c9b/saved_model.pb
    	new file:   models/model-1601426331-bbb8c9b/variables/variables.data-00000-of-00001
    	new file:   models/model-1601426331-bbb8c9b/variables/variables.index
    	modified:   models/results.json

--- astronet/t2/evaluate.py
+++ astronet/t2/evaluate.py
@@ -2,6 +2,24 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
+import numpy as np
+import json
+import tensorflow as tf
+
+from pathlib import Path
+print("File      Path:", Path(__file__).absolute())
+print("Parent of Directory Path:", Path().absolute().parent)
+
+RANDOM_SEED = 42
+
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+with open(str(Path().absolute()) + '/models/results.json') as f:
+    events = json.load(f)
+    event = max(events['training_result'], key=lambda ev: ev['value'])
+    print(event)
+
 # TODO
 # 1. Load saved model.
 # 2. Run inference.
