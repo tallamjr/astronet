123d0138f60a09c86c9541fa8d564008150c4488
Tarek Allam <t.allam.jr@gmail.com>
2020-10-02 13:27:01 +0100

    Change date format for logs
    	modified:   utils.py

--- astronet/t2/utils.py
+++ astronet/t2/utils.py
@@ -6,8 +6,6 @@ import tensorflow as tf
 import logging
 
 from pathlib import Path
-# print("File      Path:", Path(__file__).absolute())
-# print("Parent of Directory Path:", Path().absolute().parent)
 from astronet.t2.preprocess import robust_scale
 
 
@@ -47,13 +45,8 @@ def t2_logger(name, level="INFO"):
     FORMAT += "{%(filename)s:%(lineno)d} "
     FORMAT += "%(levelname)s "
     FORMAT += "- %(message)s"
-    # FORMAT = "[%(asctime)-15s]"
-    # FORMAT += "[%(filename)s:"
-    # FORMAT += "%(lineno)s] -- "
-    # FORMAT += "%(levelname)s "
-    # FORMAT += "%(message)s"
     # Date format
-    DATEFORMAT = "%y-%m-%d.%H:%M:%S"
+    DATEFORMAT = "%y-%m-%d %H:%M:%S"
 
     formatter = logging.Formatter(fmt=FORMAT, datefmt=DATEFORMAT)
     # Add formatter to ch
