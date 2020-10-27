f3ab300608fd1bc0efb45a65076989f0beec926a
Tarek Allam <t.allam.jr@gmail.com>
2020-10-27 13:18:11 +0000

    Add verbatim from C. Alves, code for GP fits
    	modified:   preprocess.py

--- astronet/t2/preprocess.py
+++ astronet/t2/preprocess.py
@@ -1,3 +1,136 @@
+import george
+import numpy as np
+import pandas as pd
+from astropy.table import Table, vstack
+import scipy.optimize as op
+from functools import partial
+
+
+def fit_2d_gp(obj_data, return_kernel=False, **kwargs):
+    """Fit a 2D Gaussian process.
+
+    If required, predict the GP at evenly spaced points along a light curve.
+
+    Parameters
+    ----------
+    obj_data : pandas.core.frame.DataFrame or astropy.table.Table
+        Time, flux and flux error of the data (specific filter of an object).
+    return_kernel : Bool, default = False
+        Whether to return the used kernel.
+    kwargs : dict
+        Additional keyword arguments that are ignored at the moment. We allow
+        additional keyword arguments so that the various functions that
+        call this one can be called with the same arguments.
+
+    Returns
+    -------
+    kernel: george.gp.GP.kernel, optional
+        The kernel used to fit the GP.
+    gp_predict : functools.partial of george.gp.GP
+        The GP instance that was used to fit the object.
+    """
+    guess_length_scale = 20.0  # a parameter of the Matern32Kernel
+
+    #obj_data = preprocess_obs(obj_data, **kwargs)  # preprocess obs
+
+    obj_times = obj_data.mjd
+    obj_flux = obj_data.flux
+    obj_flux_error = obj_data.flux_error
+    obj_wavelengths = obj_data['filter'].map(pb_wavelengths)
+
+    def neg_log_like(p):  # Objective function: negative log-likelihood
+        gp.set_parameter_vector(p)
+        loglike = gp.log_likelihood(obj_flux, quiet=True)
+        return -loglike if np.isfinite(loglike) else 1e25
+
+    def grad_neg_log_like(p):  # Gradient of the objective function.
+        gp.set_parameter_vector(p)
+        return -gp.grad_log_likelihood(obj_flux, quiet=True)
+
+    # Use the highest signal-to-noise observation to estimate the scale. We
+    # include an error floor so that in the case of very high
+    # signal-to-noise observations we pick the maximum flux value.
+    signal_to_noises = np.abs(obj_flux) / np.sqrt(
+        obj_flux_error ** 2 + (1e-2 * np.max(obj_flux)) ** 2
+    )
+    scale = np.abs(obj_flux[signal_to_noises.idxmax()])
+
+    kernel = (0.5 * scale) ** 2 * george.kernels.Matern32Kernel([
+        guess_length_scale ** 2, 6000 ** 2], ndim=2)
+    kernel.freeze_parameter("k2:metric:log_M_1_1")
+
+    gp = george.GP(kernel)
+    default_gp_param = gp.get_parameter_vector()
+    x_data = np.vstack([obj_times, obj_wavelengths]).T
+    gp.compute(x_data, obj_flux_error)
+
+    bounds = [(0, np.log(1000 ** 2))]
+    bounds = [(default_gp_param[0] - 10, default_gp_param[0] + 10)] + bounds
+    results = op.minimize(neg_log_like, gp.get_parameter_vector(),
+                          jac=grad_neg_log_like, method="L-BFGS-B",
+                          bounds=bounds, tol=1e-6)
+
+    if results.success:
+        gp.set_parameter_vector(results.x)
+    else:
+        # Fit failed. Print out a warning, and use the initial guesses for fit
+        # parameters.
+        obj = obj_data['object_id'][0]
+        print("GP fit failed for {}! Using guessed GP parameters.".format(obj))
+        gp.set_parameter_vector(default_gp_param)
+
+    gp_predict = partial(gp.predict, obj_flux)
+
+    if return_kernel:
+        return kernel, gp_predict
+    else:
+        return gp_predict
+
+
+def predict_2d_gp(gp_predict, gp_times, gp_wavelengths):
+    """Outputs the predictions of a Gaussian Process.
+
+    Parameters
+    ----------
+    gp_predict : functools.partial of george.gp.GP
+        The GP instance that was used to fit the object.
+    gp_times : numpy.ndarray
+        Times to evaluate the Gaussian Process at.
+    gp_wavelengths : numpy.ndarray
+        Wavelengths to evaluate the Gaussian Process at.
+
+    Returns
+    -------
+    obj_gps : pandas.core.frame.DataFrame, optional
+        Time, flux and flux error of the fitted Gaussian Process.
+    """
+    unique_wavelengths = np.unique(gp_wavelengths)
+    number_gp = len(gp_times)
+    obj_gps = []
+    for wavelength in unique_wavelengths:
+        gp_wavelengths = np.ones(number_gp) * wavelength
+        pred_x_data = np.vstack([gp_times, gp_wavelengths]).T
+        pb_pred, pb_pred_var = gp_predict(pred_x_data, return_var=True)
+        # stack the GP results in a array momentarily
+        obj_gp_pb_array = np.column_stack((gp_times, pb_pred, np.sqrt(pb_pred_var)))
+        obj_gp_pb = Table(
+            [
+                obj_gp_pb_array[:, 0],
+                obj_gp_pb_array[:, 1],
+                obj_gp_pb_array[:, 2],
+                [wavelength] * number_gp,
+            ],
+            names=["mjd", "flux", "flux_error", "filter"],
+        )
+        if len(obj_gps) == 0:  # initialize the table for 1st passband
+            obj_gps = obj_gp_pb
+        else:  # add more entries to the table
+            obj_gps = vstack((obj_gps, obj_gp_pb))
+
+    obj_gps = obj_gps.to_pandas()
+    return obj_gps
+
+
 def robust_scale(df_train, df_val, df_test, scale_columns):
     from sklearn.preprocessing import RobustScaler
 
@@ -28,3 +161,5 @@ def one_hot_encode(y_train, y_val, y_test):
     y_test = enc.transform(y_test)
 
     return enc, y_train, y_val, y_test
+
+
