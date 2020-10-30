20781430b0024139bcb379a92cbf9ed726ec380e
Tarek Allam <t.allam.jr@gmail.com>
2020-10-30 12:21:17 +0000

    Add passband values and colors for PLAsTiCC plots
    	new file:   astronet/t2/constants.py

---
+++ astronet/t2/constants.py
@@ -0,0 +1,19 @@
+# Central passbands wavelengths
+pb_wavelengths = {
+    "lsstu": 3685.0,
+    "lsstg": 4802.0,
+    "lsstr": 6231.0,
+    "lssti": 7542.0,
+    "lsstz": 8690.0,
+    "lssty": 9736.0,
+}
+
+# Colours for the plotting light curves
+pb_colors = {
+    "lsstu": "#984ea3",  # Purple: https://www.color-hex.com/color/984ea3
+    "lsstg": "#4daf4a",  # Green: https://www.color-hex.com/color/4daf4a
+    "lsstr": "#e41a1c",  # Red: https://www.color-hex.com/color/e41a1c
+    "lssti": "#377eb8",  # Blue: https://www.color-hex.com/color/377eb8
+    "lsstz": "#ff7f00",  # Orange: https://www.color-hex.com/color/ff7f00
+    "lssty": "#e3c530",  # Yellow: https://www.color-hex.com/color/e3c530
+}
