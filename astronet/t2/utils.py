66702c9f777ae0136ed821ae38d344bc83e21efa
Tarek Allam <t.allam.jr@gmail.com>
2020-11-04 14:21:45 +0000

    Investigate performance of timesteps=200
    Since this is the timesteps used for widsm_2010 which
    obtains better performance and that the original data is sampled at
    20Hz, so for a 10 second sample, there would be 200 points.
    
    Ref:
            - https://archive.ics.uci.edu/ml/machine-learning-databases/00507/WISDM-dataset-description.pdf
    
    > 4.1 The Data Transformation Process
    > The basic data transformation process utilized to form the labeled
    > examples provided with this data set has been used by our WISDM Lab
    > since 2010 and has been used in many research papers-- although nor-
    > mally on smaller data sets. The process was described in our first
    > article on activity recognition [2], although the transformation process
    > applied to generate the examples in this data set include some
    > additional features.
    >
    > The raw time series data for each sensor (per sub- ject and per
    > activity) is divided into 10-second non- overlapping segments and then
    > high-level features are generated based on the 200 (10s x 20 readings/s)
    > readings contained within each segment. A 10-second window was chosen
    > because we felt that it provided sufficient time to capture several
    > repetitions of those actitions that involve repititious movements, and
    > still is small enough to provide quick response (i.e., a pre- diction
    > every 10 seconds).
    
            - https://www.cis.fordham.edu/wisdm/includes/files/sensorKDD-2010.pdf
    
    > 2.2 Feature Generation & Data Transformation
    > Standard classification algorithms cannot be directly applied to
    > raw time-series accelerometer data. Instead, we first must transform the
    > raw time series data into examples [18]. To accomplish
    > this we divided the data into 10-second segments and then generated
    > features that were based on the 200 readings contained
    > within each 10-second segment. We refer to the duration of each
    > segment as the example duration (ED). We chose a 10-second ED
    > because we felt that it provided sufficient time to capture several
    > repetitions of the (repetitive) motions involved in some of the six
    > activities. Although we have not performed experiments to determine the
    > optimal example duration value, we did compare the
    > results for a 10-second and 20-second ED and the 10-second ED
    > yielded slightly better results (as well as twice as many training
    > examples).
    
    Within bin/t2, SBATCH --exclusive has been chosen to prevent previous
    error of cluster time out. See 563c930ae5d138bf
    
    	modified:   astronet/t2/utils.py
    	modified:   bin/t2

--- astronet/t2/utils.py
+++ astronet/t2/utils.py
@@ -155,7 +155,7 @@ def load_wisdm_2010(timesteps=200, step=40):
 #  TODO:
 # Investigate performance of timesteps=200 for wisdm_2019 since this is the timesteps used for
 # widsm_2010 which obtains better performance.
-def load_wisdm_2019(timesteps=100, step=40):
+def load_wisdm_2019(timesteps=200, step=40):
 
     RANDOM_SEED = 42
     np.random.seed(RANDOM_SEED)
