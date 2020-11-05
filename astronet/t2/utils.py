7198b5355cdd9c3a99a98c867e75a8d9a318e905
Tarek Allam <t.allam.jr@gmail.com>
2020-11-05 15:17:12 +0000

    Add implementation of plasticc log loss metric
    Refs:
        - https://github.com/jfpuget/Kaggle_PLAsTiCC/blob/master/code/lgb_best.ipynb
        - https://astrorapid.readthedocs.io/en/latest/_modules/astrorapid/classifier_metrics.html
        - https://www.kaggle.com/c/PLAsTiCC-2018/overview/evaluation
    
    	modified:   utils.py

--- astronet/t2/utils.py
+++ astronet/t2/utils.py
@@ -408,3 +408,75 @@ def load_plasticc(timesteps=20, step=20):
     # assert y_train.shape == (95603, 1)
 
     return X_train, y_train, X_val, y_val, X_test, y_test
+
+
+def plasticc_log_loss(y_true, probs):
+
+    """Weighted LogLoss used in the PLAsTiCC kaggle challenge.
+
+    Parameters
+    ----------
+    y_true: np.array of shape (# samples,)
+        Array of the true classes. This may require enc.inverse_transform(y_true) if values are
+        currently one-hot encoded
+    probs : np.array of shape (# samples, # features)
+        Class probabilities for each sample.
+
+    Returns
+    -------
+    float
+        Weighted log loss used for the Kaggle challenge
+
+    References
+    ----------
+    - https://github.com/jfpuget/Kaggle_PLAsTiCC/blob/master/code/lgb_best.ipynb
+    - https://astrorapid.readthedocs.io/en/latest/_modules/astrorapid/classifier_metrics.html
+    - https://www.kaggle.com/c/PLAsTiCC-2018/overview/evaluation
+    """
+    predictions = probs.copy()
+    labels = np.unique(y_true)
+
+    weights_dict = {
+        6: 1 / 18,
+        15: 1 / 9,
+        16: 1 / 18,
+        42: 1 / 18,
+        52: 1 / 18,
+        53: 1 / 18,
+        62: 1 / 18,
+        64: 1 / 9,
+        65: 1 / 18,
+        67: 1 / 18,
+        88: 1 / 18,
+        90: 1 / 18,
+        92: 1 / 18,
+        95: 1 / 18,
+        99: 1 / 19,
+        1: 1 / 18,
+        2: 1 / 18,
+        3: 1 / 18,
+    }
+
+    # sanitize predictions
+    epsilon = (
+        sys.float_info.epsilon
+    )  # this is machine dependent but essentially prevents log(0)
+    predictions = np.clip(predictions, epsilon, 1.0 - epsilon)
+    predictions = predictions / np.sum(predictions, axis=1)[:, np.newaxis]
+    predictions = np.log(predictions)  # logarithm because we want a log loss
+
+    class_logloss, weights = [], []  # initialize the classes logloss and weights
+    for i in range(np.shape(predictions)[1]):  # run for each class
+        current_label = labels[i]
+        result = np.average(
+            predictions[y_true.ravel() == current_label, i]
+        )
+        # works like a boolean mask to provide results for current class. ravel() required to fix
+        # IndexError: result = np.average(predictions[y_true==current_label, i]) # only those
+        # events are from that class IndexError: too many indices for array: array is 2-dimensional,
+        # but 3 were indexed
+
+        class_logloss.append(result)
+        weights.append(weights_dict[current_label])
+
+    return -1 * np.average(class_logloss, weights=weights)
