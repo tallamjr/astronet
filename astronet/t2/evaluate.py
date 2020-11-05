bf988071d093a68675afee6cd07ed011ffcf675f
Tarek Allam <t.allam.jr@gmail.com>
2020-11-05 14:00:02 +0000

    [FIXUP] Try different implementations of wlogloss
    	modified:   evaluate.py

--- astronet/t2/evaluate.py
+++ astronet/t2/evaluate.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import numpy as np
+import pandas as pd
 import shutil
 import sys
 import tensorflow as tf
@@ -74,9 +75,116 @@ model = keras.models.load_model(f"{asnwd}/astronet/t2/models/{args.dataset}/mode
 
 model.evaluate(X_test, y_test)
 y_pred = model.predict(X_test)
+
 cm = confusion_matrix(enc.inverse_transform(y_test), enc.inverse_transform(y_pred))
 print(cm / np.sum(cm, axis=1, keepdims=1))
 
 print("             Results for Test Set\n\n" +
       classification_report(enc.inverse_transform(y_pred),
                             enc.inverse_transform(y_test)))
+
+
+probs = y_pred
+print(f"PROBS --> {probs}")
+y_true = enc.inverse_transform(y_test)
+print(f"y_true --> {y_true}")
+
+"""Implementation of weighted log loss used for the Kaggle challenge.
+
+Parameters
+----------
+y_true: np.array of shape (# samples,)
+    Array of the true classes
+probs : np.array of shape (# samples, # features)
+    Class probabilities for each sample. The order of the classes corresponds to
+    that in the attribute `classes_` of the classifier used.
+
+Returns
+-------
+float
+    Weighted log loss used for the Kaggle challenge
+"""
+predictions = probs.copy()
+labels = np.unique(y_true) # assumes the probabilities are also ordered in the same way
+# labels = np.unique(enc.inverse_transform(y_test))
+print(f"LABELS--> {labels}")
+
+weights_dict = {
+    6: 1 / 18,
+    15: 1 / 9,
+    16: 1 / 18,
+    42: 1 / 18,
+    52: 1 / 18,
+    53: 1 / 18,
+    62: 1 / 18,
+    64: 1 / 9,
+    65: 1 / 18,
+    67: 1 / 18,
+    88: 1 / 18,
+    90: 1 / 18,
+    92: 1 / 18,
+    95: 1 / 18,
+    99: 1 / 19,
+    1: 1 / 18,
+    2: 1 / 18,
+    3: 1 / 18,
+}
+
+# sanitize predictions
+epsilon = sys.float_info.epsilon  # this is machine dependent but essentially prevents log(0)
+predictions = np.clip(predictions, epsilon, 1.0 - epsilon)
+print(f"PREDICTIONS--> {predictions}")
+print(f"PREDICTIONS.SHAPE--> {predictions.shape}")
+predictions = predictions / np.sum(predictions, axis=1)[:, np.newaxis]
+print(f"PREDICTIONS.SHAPE--> {predictions.shape}")
+predictions = np.log(predictions) # logarithm because we want a log loss
+print(f"PREDICTIONS.LOG.SHAPE--> {predictions.shape}")
+
+class_logloss, weights = [], [] # initialize the classes logloss and weights
+print(f"PREDICTIONS.[1].SHAPE--> {np.shape(predictions)[1]}")
+for i in range(np.shape(predictions)[1]): # run for each class
+    current_label = labels[i]
+    # import pdb; pdb.set_trace()
+    result = np.average(predictions[y_true.ravel()==current_label, i]) # only those events are from that class
+    print(f"RESULT.SHAPE--> {result}")
+    class_logloss.append(result)
+    weights.append(weights_dict[current_label])
+print(class_logloss)
+print(weights)
+
+print(-1 * np.average(class_logloss, weights=weights))
+print("=" * shutil.get_terminal_size((80, 20))[0])
+
+class_weight = {
+    42: 1,
+    62: 1,
+    90: 1,
+}
+"""
+@author olivier https://www.kaggle.com/ogrellier
+multi logloss for PLAsTiCC challenge
+"""
+# class_weights taken from Giba's topic : https://www.kaggle.com/titericz
+# https://www.kaggle.com/c/PLAsTiCC-2018/discussion/67194
+# with Kyle Boone's post https://www.kaggle.com/kyleboone
+
+y_p = probs
+# Trasform y_true in dummies
+y_ohe = pd.get_dummies(y_true.ravel())
+# Normalize rows and limit y_preds to 1e-15, 1-1e-15
+y_p = np.clip(a=y_p, a_min=1e-15, a_max=1 - 1e-15)
+# Transform to log
+y_p_log = np.log(y_p)
+# Get the log for ones, .values is used to drop the index of DataFrames
+# Exclude class 99 for now, since there is no class99 in the training set
+# we gave a special process for that class
+y_log_ones = np.sum(y_ohe.values * y_p_log, axis=0)
+# Get the number of positives for each class
+nb_pos = y_ohe.sum(axis=0).values.astype(float)
+# Weight average and divide by the number of positives
+class_arr = np.array([class_weight[k] for k in sorted(class_weight.keys())])
+y_w = y_log_ones * class_arr / nb_pos
+
+loss = - np.sum(y_w) / np.sum(class_arr)
+# assert loss == -1 * np.average(class_logloss, weights=weights)
+print(loss)
