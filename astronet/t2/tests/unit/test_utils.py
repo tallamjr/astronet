560b956dbc09b114a551e55e90a41ce017bf17a3
Tarek Allam <t.allam.jr@gmail.com>
2020-11-06 13:36:14 +0000

    [WIP] Update tests to check fixed window sizes

--- astronet/t2/tests/unit/test_utils.py
+++ astronet/t2/tests/unit/test_utils.py
@@ -6,7 +6,7 @@ from astronet.t2.utils import load_wisdm_2010, load_wisdm_2019, load_plasticc
 
 def test_load_wisdm_2010():
 
-    X_train, y_train, X_val, y_val, X_test, y_test = load_wisdm_2010()
+    X_train, y_train, X_val, y_val, X_test, y_test = load_wisdm_2010(timesteps=200, step=200)
 
     assert X_train.shape == (4392, 200, 3)
     assert X_val.shape == (823, 200, 3)
@@ -35,7 +35,7 @@ def test_load_wisdm_2019():
 
 def test_load_plasticc():
 
-    X_train, y_train, X_val, y_val, X_test, y_test = load_plasticc()
+    X_train, y_train, X_val, y_val, X_test, y_test = load_plasticc(timesteps=20, step=20)
 
     assert X_train.shape == (15959, 20, 6)
     assert X_val.shape == (2992, 20, 6)
