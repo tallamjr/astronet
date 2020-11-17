e83ab8f16afd14548cf0be9620f8799c1f942a58
Tarek Allam <t.allam.jr@gmail.com>
2020-11-17 23:50:19 +0000

    [WIP] Tidy up file. TODO: Needs rework to correct dims in confusion matrix
    	modified:   evaluate.py

--- astronet/t2/evaluate.py
+++ astronet/t2/evaluate.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import numpy as np
-import pandas as pd
 import shutil
 import sys
 import tensorflow as tf
@@ -11,14 +10,17 @@ from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from tensorflow import keras
 
-from astronet.t2.constants import astronet_working_directory as asnwd
-from astronet.t2.preprocess import one_hot_encode
+from astronet.t2.constants import (
+    plasticc_weights_dict,
+    astronet_working_directory as asnwd,
+)
+from astronet.t2.metrics import plasticc_log_loss, custom_log_loss
+from astronet.t2.preprocess import one_hot_encode, tf_one_hot_encode
 from astronet.t2.utils import (
     t2_logger,
     load_wisdm_2010,
     load_wisdm_2019,
     load_plasticc,
-    plasticc_log_loss,
 )
 
 try:
@@ -34,6 +36,8 @@ RANDOM_SEED = 42
 
 np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
+
+np.set_printoptions(suppress=True, formatter={"float_kind": "{:0.2f}".format})
 
 parser = argparse.ArgumentParser(description='Evaluate best performing model for a given dataset')
 
@@ -61,7 +65,32 @@ elif args.dataset == "plasticc":
 X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
 
 # One hot encode y
-enc, y_train, y_val, y_test = one_hot_encode(y_train, y_val, y_test)
+enc, sk_y_train, sk_y_val, sk_y_test = one_hot_encode(y_train, y_val, y_test)
+
+# One hot encode y with tensorflow
+tf_y_train, tf_y_val, tf_y_test = tf_one_hot_encode(y_train, y_val, y_test)
+print(y_test)
+print(sk_y_train)
+print(tf_y_train)
+
+# flat_labels = tf.reshape(y_train, [-1])
+# print(flat_labels)
+# tfenc = tf.one_hot(flat_labels, len(np.unique(y_train)))
+# print(tfenc)
+
+dct = {42: 0, 62: 1, 90: 2}
+lst = y_test.flatten().tolist()
+
+flabels = list(map(dct.get, lst))
+__y_test = tf.one_hot(flabels, len(np.unique(y_test)))
+print(f"FLABELS: {flabels}")
+
+inv_map = {v: k for k, v in dct.items()}
+print(inv_map)
+print(__y_test.numpy().tolist())
+
+flip = list(map(inv_map.get, flabels))
+print(f"FLIP: {flip}")
 
 dataset = args.dataset
 with open(f"{asnwd}/astronet/t2/models/{dataset}/results.json") as f:
@@ -69,7 +98,7 @@ with open(f"{asnwd}/astronet/t2/models/{dataset}/results.json") as f:
     if args.model:
         # Get params for model chosen with cli args
         event = next(item for item in events['training_result'] if item["name"] == args.model)
-        print(event)
+        # print(event)
     else:
         # Get params for best model with highest test accuracy
         event = max(events['training_result'], key=lambda ev: ev['value'])
@@ -79,97 +108,23 @@ model_name = event['name']
 
 model = keras.models.load_model(f"{asnwd}/astronet/t2/models/{args.dataset}/model-{model_name}")
 
-model.evaluate(X_test, y_test)
+model.evaluate(X_test, sk_y_test)
+model.evaluate(X_test, tf_y_test)
 y_probs = model.predict(X_test)
 
-cm = confusion_matrix(enc.inverse_transform(y_test), enc.inverse_transform(y_probs))
+cm = confusion_matrix(enc.inverse_transform(sk_y_test), enc.inverse_transform(y_probs))
 print(cm / np.sum(cm, axis=1, keepdims=1))
 
 print("             Results for Test Set\n\n" +
       classification_report(enc.inverse_transform(y_probs),
-                            enc.inverse_transform(y_test)))
-
-weights_dict = {
-    6: 1 / 18,
-    15: 1 / 9,
-    16: 1 / 18,
-    42: 1 / 18,
-    52: 1 / 18,
-    53: 1 / 18,
-    62: 1 / 18,
-    64: 1 / 9,
-    65: 1 / 18,
-    67: 1 / 18,
-    88: 1 / 18,
-    90: 1 / 18,
-    92: 1 / 18,
-    95: 1 / 18,
-    99: 1 / 19,
-    1: 1 / 18,
-    2: 1 / 18,
-    3: 1 / 18,
-}
+                            enc.inverse_transform(sk_y_test)))
 
 
-def custom_log_loss(y_true, y_pred):
-    """
-    References:
-    -----------
-    -
+closs = custom_log_loss(tf_y_test, y_probs)
+print(f"CUSTOM LOG LOSS {closs}")
 
-    """
+# tfloss = tf.keras.losses.categorical_crossentropy(y_test, y_probs)
+# print(np.average(tfloss.numpy()))
 
-    yc = tf.clip_by_value(y_pred, 1e-15, 1 - 1e-15)
-    loss = -(tf.reduce_mean(tf.reduce_mean(y_true * tf.math.log(yc), axis=0) * len(y_pred)))
-    # labels = np.unique(y_true)
-    # loss = tf.reduce_sum(tf.multiply(- labels, tf.math.log(y_pred))) / len(y_pred)
-    return loss
-
-
-print(custom_log_loss(y_test, y_probs))
-
-
-# def custom_plasticc_loss(y_true, y_pred):
-
-#     predictions = y_pred
-#     labels = np.unique(y_true)
-
-#     # sanitize predictions
-#     epsilon = (
-#         sys.float_info.epsilon
-#     )  # this is machine dependent but essentially prevents log(0)
-#     predictions = tf.experimental.numpy.clip(predictions, epsilon, 1.0 - epsilon)
-#     predictions = predictions / tf.experimental.numpy.sum(predictions, axis=1)[:, tf.newaxis]
-#     predictions = tf.math.log(predictions)  # logarithm because we want a log loss
-
-#     class_logloss, weights = [], []  # initialize the classes logloss and weights
-#     for i in range(np.shape(predictions)[1]):  # run for each class
-#         current_label = labels[i]
-#         result = tf.experimental.numpy.average(
-#             predictions[y_true.ravel() == current_label, i]
-#         )
-#         # works like a boolean mask to provide results for current class. ravel() required to fix
-#         # IndexError: result = np.average(predictions[y_true==current_label, i]) # only those
-#         # events are from that class IndexError: too many indices for array: array is 2-dimensional,
-#         # but 3 were indexed
-
-#         class_logloss.append(result)
-#         weights.append(weights_dict[current_label])
-
-#     return -1 * tf.experimental.numpy.average(class_logloss, weights=weights)
-
-
-# print(custom_plasticc_loss(y_test, y_pred))
-
-if args.dataset == "plasticc":
-
-    y_true = enc.inverse_transform(y_test)
-
-    loss = plasticc_log_loss(y_true, y_probs)
-    print(loss)
-
-tfloss = tf.keras.losses.categorical_crossentropy(y_test, y_probs)
-print(np.average(tfloss.numpy()))
-
-cce = tf.keras.losses.CategoricalCrossentropy()
-print(cce(y_test, y_probs).numpy())
+# cce = tf.keras.losses.CategoricalCrossentropy()
+# print(cce(y_test, y_probs).numpy())
