dc2f7161c2699e0bef0d048f15fbef7232c75bbd
Tarek Allam <t.allam.jr@gmail.com>
2020-12-03 13:06:32 +0000

    Add implementation of PositionalEncoding class
    See #36 for details.
    
    This implementation is from Géron, A., 2019. Hands-on machine learning
    with Scikit-Learn, Keras, and TensorFlow: Concepts, tools, and
    techniques to build intelligent systems. O'Reilly Media. pg. 588
    
    	modified:   astronet/t2/transformer.py

--- astronet/t2/transformer.py
+++ astronet/t2/transformer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
@@ -41,3 +42,19 @@ class ConvEmbedding(layers.Layer):
         embedding = self.conv1d(inputs)
 
         return embedding
+
+
+class PositionalEncoding(keras.layers.Layer):
+    def __init__(self, max_steps, max_dims, dtype=tf.float32, **kwargs):
+        super(PositionalEncoding).__init__(dtype=dtype, **kwargs)
+        if max_dims % 2 == 1:
+            max_dims += 1  # max_dims must be even
+        p, i = np.meshgrid(np.arange(max_steps), np.arange(max_dims // 2))
+        pos_emb = np.empty((1, max_steps, max_dims))
+        pos_emb[0, :, ::2] = np.sin(p / 10000 ** (2 * i / max_dims)).T
+        pos_emb[0, :, 1::2] = np.cos(p / 10000 ** (2 * i / max_dims)).T
+        self.positional_embedding = tf.constant(pos_emb.astype(self.dtype))
+
+    def call(self, inputs):
+        shape = tf.shape(inputs)
+        return inputs + self.positional_embedding[:, : shape[-2], : shape[-1]]
