720d80b04bb917814bc7aee0670697b6e74825c8
Tarek Allam <t.allam.jr@gmail.com>
2020-12-03 13:13:38 +0000

    Rearrange classes to be inline with architecture
    It was felt that having it in this way, it is easier to follow the steps
    of the architecture
    
    	modified:   astronet/t2/transformer.py

--- astronet/t2/transformer.py
+++ astronet/t2/transformer.py
@@ -6,31 +6,6 @@ from tensorflow.keras import layers
 from astronet.t2.attention import MultiHeadSelfAttention
 
 
-class TransformerBlock(layers.Layer):
-    # TODO: Update docstrings
-    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
-        super(TransformerBlock, self).__init__()
-        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
-        self.ffn = keras.Sequential(
-            [
-                layers.Dense(ff_dim, activation="relu"),
-                layers.Dense(embed_dim),
-            ]
-        )
-        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = layers.Dropout(rate)
-        self.dropout2 = layers.Dropout(rate)
-
-    def call(self, inputs, training):
-        attn_output = self.att(inputs)
-        attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
-        ffn_output = self.ffn(out1)
-        ffn_output = self.dropout2(ffn_output, training=training)
-        return self.layernorm2(out1 + ffn_output)
-
-
 class ConvEmbedding(layers.Layer):
     def __init__(self, num_filters, **kwargs):
         super(ConvEmbedding, self).__init__(**kwargs)
@@ -58,3 +33,28 @@ class PositionalEncoding(keras.layers.Layer):
     def call(self, inputs):
         shape = tf.shape(inputs)
         return inputs + self.positional_embedding[:, : shape[-2], : shape[-1]]
+
+
+class TransformerBlock(layers.Layer):
+    # TODO: Update docstrings
+    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+        super(TransformerBlock, self).__init__()
+        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
+        self.ffn = keras.Sequential(
+            [
+                layers.Dense(ff_dim, activation="relu"),
+                layers.Dense(embed_dim),
+            ]
+        )
+        self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
+        self.dropout1 = layers.Dropout(rate)
+        self.dropout2 = layers.Dropout(rate)
+
+    def call(self, inputs, training):
+        attn_output = self.att(inputs)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(inputs + attn_output)
+        ffn_output = self.ffn(out1)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        return self.layernorm2(out1 + ffn_output)
