27380747e9990905832d8a52ea2a115708580ed0
Tarek Allam <t.allam.jr@gmail.com>
2021-01-19 18:52:21 +0000

    Adapt Xception network from 2D to 1D operations
    	modified:   astronet/snX/layers.py

--- astronet/snX/layers.py
+++ astronet/snX/layers.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 from tensorflow.keras.layers import (
-    Conv2D,
-    SeparableConv2D,
+    Conv1D,
+    SeparableConv1D,
     Add,
     Dense,
     BatchNormalization,
     ReLU,
-    MaxPool2D,
-    GlobalAvgPool2D,
+    MaxPool1D,
+    GlobalAveragePooling1D,
 )
 
 
@@ -19,7 +19,7 @@ class ConvBatchNormBlock(tf.keras.layers.Layer):
         self.kernel_size = kernel_size
         self.strides = strides
 
-        self.conv2d = Conv2D(
+        self.conv1d = Conv1D(
             filters=self.filters,
             kernel_size=self.kernel_size,
             strides=self.strides,
@@ -30,7 +30,7 @@ class ConvBatchNormBlock(tf.keras.layers.Layer):
 
     def call(self, inputs):
         x = inputs
-        x = self.conv2d(x)
+        x = self.conv1d(x)
         x = self.batchnorm(x)
         return x
 
@@ -43,7 +43,7 @@ class SeparableConvBatchNormBlock(tf.keras.layers.Layer):
         self.kernel_size = kernel_size
         self.strides = strides
 
-        self.sep_conv_2d = SeparableConv2D(
+        self.sep_conv_1d = SeparableConv1D(
             filters=self.filters,
             kernel_size=self.kernel_size,
             strides=self.strides,
@@ -55,7 +55,7 @@ class SeparableConvBatchNormBlock(tf.keras.layers.Layer):
 
     def call(self, inputs):
         x = inputs
-        x = self.sep_conv_2d(x)
+        x = self.sep_conv_1d(x)
         x = self.batchnorm(x)
         return x
 
@@ -94,7 +94,7 @@ class EntryFlow(tf.keras.layers.Layer):
         # relu
         self.sep_conv_batchnorm_7 = SeparableConvBatchNormBlock(filters=728, kernel_size=3)
         # maxpool
-        self.maxpool = MaxPool2D(pool_size=3, strides=2, padding='same')
+        self.maxpool = MaxPool1D(pool_size=3, strides=2, padding='same')
 
         self.conv_batchnorm_5 = ConvBatchNormBlock(filters=728, kernel_size=1, strides=2)
         # add
@@ -170,7 +170,7 @@ class ExitFlow(tf.keras.layers.Layer):
         # relu
         self.sep_conv_batchnorm_exit_2 = SeparableConvBatchNormBlock(filters=1024, kernel_size=3)
         # maxpool
-        self.maxpool = MaxPool2D(pool_size=3, strides=2, padding='same')
+        self.maxpool = MaxPool1D(pool_size=3, strides=2, padding='same')
 
         self.conv_batchnorm_exit_1 = ConvBatchNormBlock(filters=1024, kernel_size=1, strides=2)
 
@@ -180,7 +180,7 @@ class ExitFlow(tf.keras.layers.Layer):
         self.sep_conv_batchnorm_exit_4 = SeparableConvBatchNormBlock(filters=2048, kernel_size=3)
         # relu
         # gap
-        self.gap = GlobalAvgPool2D()
+        self.gap = GlobalAveragePooling1D()
 
         self.dense = Dense(units=1000, activation='softmax')
 
