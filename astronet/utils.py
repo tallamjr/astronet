ea9517a9e4ad01d7a6779df0d842c9afeea5a5d8
Tarek Allam <t.allam.jr@gmail.com>
2021-02-02 13:15:23 +0000

    Replace _aug --> 000 to make int conversion easier
    This should fix df['object_id'].astype(int) error found in logs/133614.log
    
    	modified:   astronet/utils.py

--- astronet/utils.py
+++ astronet/utils.py
@@ -405,6 +405,7 @@ def __generate_augmented_plasticc_dataset_from_pickle(augmented_binary):
         adf = pd.concat([adf, aug.data[object_list[i]].to_pandas()])
 
     # adf = adf.set_index('object_id')
+    adf = adf.replace({'_aug': '000'}, regex=True)
     adf = adf.convert_dtypes()
 
     try:
