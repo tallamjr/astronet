a1c1ca9a6ebe0eb01363efe4626e3ef30ba6bbbc
Tarek Allam <t.allam.jr@gmail.com>
2021-03-09 09:47:56 +0000

    Make 3 splits instead of 2

--- sbin/split_processed_plasticc_test_set.py
+++ sbin/split_processed_plasticc_test_set.py
@@ -2,17 +2,20 @@ import numpy as np
 
 from astronet.constants import astronet_working_directory as asnwd
 
-from astronet.utils import load_dataset
+from astronet.utils import load_full_plasticc_test_from_numpy
 
-X_train, y_train, X_test, y_test, loss, Z_train, Z_test = load_dataset(
-    dataset="plasticc",
-    redshift=True,
-    testset=True
-)
+X_train, y_train, X_test, y_test, Z_train, Z_test = load_full_plasticc_test_from_numpy(redshift=True)
 
-X_train_arrays = np.array_split(X_train, 2)
+# X_train, y_train, X_test, y_test, loss, Z_train, Z_test = load_dataset(
+#     dataset="plasticc",
+#     redshift=True,
+#     testset=True
+# )
+
+X_train_arrays = np.array_split(X_train, 3)
 X_train_1 = X_train_arrays[0]
 X_train_2 = X_train_arrays[1]
+X_train_3 = X_train_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/X_train_1.npy",
     X_train_1,
@@ -21,10 +24,15 @@ np.save(
     f"{asnwd}/data/plasticc/test_set/splits/X_train_2.npy",
     X_train_2,
 )
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/X_train_3.npy",
+    X_train_3,
+)
 
-y_train_arrays = np.array_split(y_train, 2)
+y_train_arrays = np.array_split(y_train, 3)
 y_train_1 = y_train_arrays[0]
 y_train_2 = y_train_arrays[1]
+y_train_3 = y_train_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/y_train_1.npy",
     y_train_1,
@@ -33,10 +41,15 @@ np.save(
     f"{asnwd}/data/plasticc/test_set/splits/y_train_2.npy",
     y_train_2,
 )
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/y_train_3.npy",
+    y_train_3,
+)
 
-X_test_arrays = np.array_split(X_test, 2)
+X_test_arrays = np.array_split(X_test, 3)
 X_test_1 = X_test_arrays[0]
 X_test_2 = X_test_arrays[1]
+X_test_3 = X_test_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/X_test_1.npy",
     X_test_1,
@@ -45,11 +58,16 @@ np.save(
     f"{asnwd}/data/plasticc/test_set/splits/X_test_2.npy",
     X_test_2,
 )
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/X_test_3.npy",
+    X_test_3,
+)
 
 
-y_test_arrays = np.array_split(y_test, 2)
+y_test_arrays = np.array_split(y_test, 3)
 y_test_1 = y_test_arrays[0]
 y_test_2 = y_test_arrays[1]
+y_test_3 = y_test_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/y_test_1.npy",
     y_test_1,
@@ -58,10 +76,15 @@ np.save(
     f"{asnwd}/data/plasticc/test_set/splits/y_test_2.npy",
     y_test_2,
 )
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/y_test_3.npy",
+    y_test_3,
+)
 
-Z_train_arrays = np.array_split(Z_train, 2)
+Z_train_arrays = np.array_split(Z_train, 3)
 Z_train_1 = Z_train_arrays[0]
 Z_train_2 = Z_train_arrays[1]
+Z_train_3 = Z_train_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/Z_train_1.npy",
     Z_train_1,
@@ -70,10 +93,15 @@ np.save(
     f"{asnwd}/data/plasticc/test_set/splits/Z_train_2.npy",
     Z_train_2,
 )
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/Z_train_3k.npy",
+    Z_train_3,
+)
 
-Z_test_arrays = np.array_split(Z_test, 2)
+Z_test_arrays = np.array_split(Z_test, 3)
 Z_test_1 = Z_test_arrays[0]
 Z_test_2 = Z_test_arrays[1]
+Z_test_3 = Z_test_arrays[2]
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/Z_test_1.npy",
     Z_test_1,
@@ -81,4 +109,8 @@ np.save(
 np.save(
     f"{asnwd}/data/plasticc/test_set/splits/Z_test_2.npy",
     Z_test_2,
+)
+np.save(
+    f"{asnwd}/data/plasticc/test_set/splits/Z_test_3.npy",
+    Z_test_3,
 )
