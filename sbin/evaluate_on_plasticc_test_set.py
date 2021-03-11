9cb0ea7c604240b5c00ab6d651af91f346f56ee2
Tarek Allam <t.allam.jr@gmail.com>
2021-03-11 16:18:38 +0000

    Run on various batch sizes for comparison
    	modified:   sbin/evaluate_on_plasticc_test_set.py

--- sbin/evaluate_on_plasticc_test_set.py
+++ sbin/evaluate_on_plasticc_test_set.py
@@ -18,7 +18,7 @@ from tensorflow import keras
 
 from astronet.constants import astronet_working_directory as asnwd
 from astronet.preprocess import one_hot_encode
-from astronet.utils import astronet_logger, load_dataset
+from astronet.utils import astronet_logger, load_dataset, find_optimal_batch_size
 
 from astronet.metrics import WeightedLogLoss
 from astronet.visualise_results import (
@@ -53,7 +53,17 @@ X_train, y_train, X_test, y_test, loss, Z_train, Z_test = load_dataset(
                                                                         avocado=None,
                                                                         testset=True
                                                         )
+print(f"""
+        X_TRAIN: {X_train.shape}, Y_TRAIN: {y_train.shape},\n
+        X_TEST: {X_test.shape}, Y_TEST: {y_test.shape},\n
+        Z_TRAIN: {Z_train.shape}, Z_TEST: {Z_test.shape}\n
+        """
+)
+
 num_classes = y_train.shape[1]
+num_samples, timesteps, num_features = X_test.shape  # X_train.shape[1:] == (TIMESTEPS, num_features)
+BATCH_SIZE = find_optimal_batch_size(num_samples)
+print(f"BATCH_SIZE:{BATCH_SIZE}")
 
 # model_name = "1614711597-1ba461b"
 model_name = "1615402794-0.1.dev686+g8ce4a41"
@@ -78,38 +88,11 @@ model = keras.models.load_model(f"{asnwd}/astronet/{architecture}/models/{datase
                                 custom_objects={'WeightedLogLoss': WeightedLogLoss()},
                                compile=False)
 
-# dataform = "testset"
-# with open(f"{asnwd}/data/{dataform}-{dataset}.encoding", "rb") as eb:
-#     encoding = joblib.load(eb)
-# class_encoding = encoding.categories_[0]
+dataform = "testset"
+with open(f"{asnwd}/data/{dataform}-{dataset}.encoding", "rb") as eb:
+    encoding = joblib.load(eb)
+class_encoding = encoding.categories_[0]
 
-# if dataset == "plasticc":
-#     class_mapping = {
-#         90: "SNIa",
-#         67: "SNIa-91bg",
-#         52: "SNIax",
-#         42: "SNII",
-#         62: "SNIbc",
-#         95: "SLSN-I",
-#         15: "TDE",
-#         64: "KN",
-#         88: "AGN",
-#         92: "RRL",
-#         65: "M-dwarf",
-#         16: "EB",
-#         53: "Mira",
-#         6: "$\mu$-Lens-Single",
-#     }
-#     class_encoding
-#     class_names = list(np.vectorize(class_mapping.get)(class_encoding))
-# else:
-
-#     class_names = class_encoding
-from sklearn.preprocessing import OneHotEncoder
-enc = OneHotEncoder(handle_unknown="ignore", sparse=False)
-enc = enc.fit(y_test)
-# y_full_test_true_no_99 = enc.transform(y_full_test_no_99)
-class_encoding = enc.categories_[0]
 if dataset == "plasticc":
     class_mapping = {
         90: "SNIa",
@@ -130,35 +113,67 @@ if dataset == "plasticc":
     class_encoding
     class_names = list(np.vectorize(class_mapping.get)(class_encoding))
 else:
+
     class_names = class_encoding
+# from sklearn.preprocessing import OneHotEncoder
+# enc = OneHotEncoder(handle_unknown="ignore", sparse=False)
+# enc = enc.fit(y_test)
+# # y_full_test_true_no_99 = enc.transform(y_full_test_no_99)
+# class_encoding = enc.categories_[0]
+# if dataset == "plasticc":
+#     class_mapping = {
+#         90: "SNIa",
+#         67: "SNIa-91bg",
+#         52: "SNIax",
+#         42: "SNII",
+#         62: "SNIbc",
+#         95: "SLSN-I",
+#         15: "TDE",
+#         64: "KN",
+#         88: "AGN",
+#         92: "RRL",
+#         65: "M-dwarf",
+#         16: "EB",
+#         53: "Mira",
+#         6: "$\mu$-Lens-Single",
+#     }
+#     class_encoding
+#     class_names = list(np.vectorize(class_mapping.get)(class_encoding))
+# else:
+#     class_names = class_encoding
 
 from collections import Counter
 from pandas.core.common import flatten
 
 # y_true = encoding.inverse_transform(y_train)
-y_true = enc.inverse_transform(y_train)
+y_true = encoding.inverse_transform(y_train)
 print(Counter(list(flatten(y_true))))
 
-# logloss = event["model_evaluate_on_test_loss"]
-# acc = event["model_evaluate_on_test_acc"]
-# print(f"LogLoss on Test Set: {logloss}, Accuracy on Test Set: {acc}")
+logloss = event["model_evaluate_on_test_loss"]
+acc = event["model_evaluate_on_test_acc"]
+print(f"LogLoss on Test Set: {logloss}, Accuracy on Test Set: {acc}")
 
+print("Running predictions")
 wloss = WeightedLogLoss()
-
 y_preds = model.predict([X_test, Z_test])
 print(f"LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
+y_preds = model.predict([X_test, Z_test], batch_size=BATCH_SIZE)
+print(f"LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
+y_preds = model.predict([X_test, Z_test], batch_size=num_samples)
+print(f"LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
 
+# Train predictions
 y_preds_train = model.predict([X_train, Z_train])
 print(f"LL-Train: {wloss(y_train, y_preds_train).numpy():.2f}")
 
-
+print("Plotting figures...")
 cmap = sns.light_palette("Navy", as_cmap=True)
 plot_confusion_matrix(
     dataset,
     model_name,
     y_test,
     y_preds,
-    enc,
+    encoding,
     class_names,  # enc.categories_[0]
     save=True,
     cmap=cmap
