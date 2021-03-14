d600c3788039c4b258c610381d4eb5f394652b97
Tarek Allam <t.allam.jr@gmail.com>
2021-03-14 12:40:36 +0000

    Reduce max batch_size even further JIC for OOM
    	modified:   astronet/utils.py
    	modified:   bin/plasticc-myriad

--- astronet/utils.py
+++ astronet/utils.py
@@ -80,7 +80,7 @@ def find_optimal_batch_size(training_set_length):
     if (training_set_length < 10000):
         batch_size_list = [16, 32, 64]
     else:
-        batch_size_list = [512, 1024, 2028, 4096, 8192]
+        batch_size_list = [512, 1024, 2028, 4096]
     ratios = []
     for batch_size in batch_size_list:
 
