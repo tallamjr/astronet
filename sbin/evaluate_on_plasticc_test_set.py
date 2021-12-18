f75b7bf6f25966d044ffc8f856cbdc549467d2de
Tarek Allam <t.allam.jr@gmail.com>
2021-12-18 10:46:02 +0000

    Clean and tidy file of unused statements
    	modified:   sbin/evaluate_on_plasticc_test_set.py

--- sbin/evaluate_on_plasticc_test_set.py
+++ sbin/evaluate_on_plasticc_test_set.py
@@ -1,23 +1,15 @@
 import argparse
 import joblib
 import json
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-import shutil
 import sys
 import seaborn as sns
 import tensorflow as tf
 
-from itertools import cycle
-from numpy import interp
-from pathlib import Path
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_curve, auc
 from tensorflow import keras
 
 from astronet.constants import astronet_working_directory as asnwd
-from astronet.preprocess import one_hot_encode
 from astronet.utils import astronet_logger, load_dataset, find_optimal_batch_size
 
 from astronet.metrics import WeightedLogLoss
@@ -63,13 +55,6 @@ class Plots(object):
 #                                                                         avocado=None,
 #                                                                         testset=True
 #                                                         )
-# print(f"""
-#         X_TRAIN: {X_train.shape}, Y_TRAIN: {y_train.shape},\n
-#         X_TEST: {X_test.shape}, Y_TEST: {y_test.shape},\n
-#         Z_TRAIN: {Z_train.shape}, Z_TEST: {Z_test.shape}\n
-#         """
-# )
-
         X_test = np.load(
                 f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
         )
@@ -80,33 +65,19 @@ class Plots(object):
                 f"{asnwd}/data/plasticc/test_set/infer/Z_test.npy",
         )
 
-        # num_classes = y_train.shape[1]
-        num_classes = y_test.shape[1]
+        print(f"X_TEST: {X_test.shape}, Y_TEST: {y_test.shape}, Z_TEST: {Z_test.shape}")
+
         num_samples, timesteps, num_features = X_test.shape  # X_train.shape[1:] == (TIMESTEPS, num_features)
         BATCH_SIZE = find_optimal_batch_size(num_samples)
         print(f"BATCH_SIZE:{BATCH_SIZE}")
 
         if self.redshift is not None:
             inputs = [X_test, Z_test]
-        # model_name = "1614711597-1ba461b"
-        # model_name = "1615402794-0.1.dev686+g8ce4a41"
-        # model_name = "1615495431-0.1.dev701+g2539a82.d20210311" # <-- 0.505
-        # model_name = "1619538848-0.1.dev753+g8c73954"
-        # model_name = "1613551066-32f3933"
-        # model_name = None
-        # model_name = "1619624444-0.1.dev765+g7c90cbb.d20210428"
             results_filename = f"{asnwd}/astronet/{architecture}/models/{dataset}/results_with_z.json"
         else:
             inputs = X_test
             results_filename = f"{asnwd}/astronet/{architecture}/models/{dataset}/results_with_z.json"
-            #     else:
-            #         # Get params for best model with lowest loss
-            #         event = min(
-            #             (item for item in events["training_result"] if item["augmented"] is None),
-            #                 key=lambda ev: ev["model_evaluate_on_test_loss"],
-            #             )
 
-            #         event = min(events['training_result'], key=lambda ev: ev['model_evaluate_on_test_loss'])
         with open(results_filename) as f:
             events = json.load(f)
             if self.model_name is not None:
@@ -147,33 +118,6 @@ class Plots(object):
             class_names = list(np.vectorize(class_mapping.get)(class_encoding))
         else:
             class_names = class_encoding
-        # from sklearn.preprocessing import OneHotEncoder
-        # enc = OneHotEncoder(handle_unknown="ignore", sparse=False)
-        # enc = enc.fit(y_test)
-        # # y_full_test_true_no_99 = enc.transform(y_full_test_no_99)
-        # class_encoding = enc.categories_[0]
-        # if dataset == "plasticc":
-        #     class_mapping = {
-        #         90: "SNIa",
-        #         67: "SNIa-91bg",
-        #         52: "SNIax",
-        #         42: "SNII",
-        #         62: "SNIbc",
-        #         95: "SLSN-I",
-        #         15: "TDE",
-        #         64: "KN",
-        #         88: "AGN",
-        #         92: "RRL",
-        #         65: "M-dwarf",
-        #         16: "EB",
-        #         53: "Mira",
-        #         6: "$\mu$-Lens-Single",
-        #     }
-        #     class_encoding
-        #     class_names = list(np.vectorize(class_mapping.get)(class_encoding))
-        # else:
-        #     class_names = class_encoding
-
         from collections import Counter
         from pandas.core.common import flatten
 
@@ -233,7 +177,7 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--dataset", default="plasticc",
             help="Choose which dataset to use: This is fixed for plasticc for now")
 
-    parser.add_argument('-m', '--model', default=None,
+    parser.add_argument('-m', '--model', default="1619624444-0.1.dev765+g7c90cbb.d20210428",
             help='Name of tensorflow.keras model, i.e. model-<timestamp>-<hash>')
 
     parser.add_argument("-z", "--redshift", default=None,
@@ -257,22 +201,3 @@ if __name__ == "__main__":
         architecture=architecture, dataset=dataset, model_name=model_name, redshift=redshift
     )
     plotting()
-# from pathlib import Path
-# for filename in Path(f"{asnwd}/logs").rglob(f"training-*-{model_name}.log"):
-#     logfile = filename
-# event = pd.read_csv(logfile)
-
-# cmap = sns.light_palette("Navy", as_cmap=True)
-# plot_confusion_matrix(
-#     dataset,
-#     model_name,
-#     y_full_test_true_no_99,
-#     y_preds,
-#     enc,
-#     class_names,  # enc.categories_[0]
-#     save=True,
-#     cmap=cmap
-# )
-
-# plot_multiROC(dataset, model_name, model, [X_full_test_no_99, Z_full_test_no_99],
-#         y_full_test_true_no_99, class_names, save=True)
