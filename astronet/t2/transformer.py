bdbfc64b679d02c77b19d80eabfd76718140d7f4
Tarek Allam <t.allam.jr@gmail.com>
2022-02-03 14:53:21 +0000

    Reformat with black
    	modified:   transformer.py

--- astronet/t2/transformer.py
+++ astronet/t2/transformer.py
@@ -27,13 +27,13 @@ class PositionalEncoding(keras.layers.Layer):
             max_dims += 1  # max_dims must be even
         p, i = np.meshgrid(np.arange(max_steps), np.arange(max_dims // 2))
         pos_emb = np.empty((1, max_steps, max_dims))
-        pos_emb[0, :, ::2] = np.sin(p / 10000**(2 * i / max_dims)).T
-        pos_emb[0, :, 1::2] = np.cos(p / 10000**(2 * i / max_dims)).T
+        pos_emb[0, :, ::2] = np.sin(p / 10000 ** (2 * i / max_dims)).T
+        pos_emb[0, :, 1::2] = np.cos(p / 10000 ** (2 * i / max_dims)).T
         self.positional_embedding = tf.constant(pos_emb.astype(self.dtype))
 
     def call(self, inputs):
         shape = tf.shape(inputs)
-        return inputs + self.positional_embedding[:, :shape[-2], :shape[-1]]
+        return inputs + self.positional_embedding[:, : shape[-2], : shape[-1]]
 
 
 class RelativePositionEmbedding(tf.keras.layers.Layer):
@@ -50,11 +50,7 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
     max_timescale: Maximum scale that will be applied at each position.
     """
 
-    def __init__(self,
-               hidden_size,
-               min_timescale=1.0,
-               max_timescale=1.0e4,
-               **kwargs):
+    def __init__(self, hidden_size, min_timescale=1.0, max_timescale=1.0e4, **kwargs):
         # We need to have a default dtype of float32, since the inputs (which Keras
         # usually uses to infer the dtype) will always be int32.
         # We compute the positional encoding in float32 even if the model uses
@@ -86,14 +82,16 @@ class RelativePositionEmbedding(tf.keras.layers.Layer):
         position = tf.cast(tf.range(length), tf.float32)
         num_timescales = self._hidden_size // 2
         min_timescale, max_timescale = self._min_timescale, self._max_timescale
-        log_timescale_increment = math.log(float(max_timescale) / float(min_timescale)) / (
-            tf.cast(num_timescales, tf.float32) - 1
-        )
+        log_timescale_increment = math.log(
+            float(max_timescale) / float(min_timescale)
+        ) / (tf.cast(num_timescales, tf.float32) - 1)
         inv_timescales = min_timescale * tf.exp(
             tf.cast(tf.range(num_timescales), tf.float32) * -log_timescale_increment
         )
         scaled_time = tf.expand_dims(position, 1) * tf.expand_dims(inv_timescales, 0)
-        position_embeddings = tf.concat([tf.sin(scaled_time), tf.cos(scaled_time)], axis=1)
+        position_embeddings = tf.concat(
+            [tf.sin(scaled_time), tf.cos(scaled_time)], axis=1
+        )
         return inputs + position_embeddings
 
 
@@ -118,11 +116,15 @@ class TransformerBlock(layers.Layer):
         # Sublayer 1
         attn_output = self.att(inputs)  # (batch_size, input_seq_len, d_model)
         attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)  # Residual connection, (batch_size, input_seq_len, d_model)
+        out1 = self.layernorm1(
+            inputs + attn_output
+        )  # Residual connection, (batch_size, input_seq_len, d_model)
 
         # Sublayer 2
         ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
         ffn_output = self.dropout2(ffn_output, training=training)
-        out2 = self.layernorm2(out1 + ffn_output)  # Residual connection, # (batch_size, input_seq_len, d_model)
+        out2 = self.layernorm2(
+            out1 + ffn_output
+        )  # Residual connection, # (batch_size, input_seq_len, d_model)
 
         return out2  # (batch_size, input_seq_len, d_model)
