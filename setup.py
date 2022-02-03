40847118e1bda810b78d8ed3e530682fc74fca77
Tarek Allam <t.allam.jr@gmail.com>
2022-02-03 13:09:53 +0000

    Add classifiers to setup.py
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
 
 __FALLBACK_VERSION__ = "0.1"
 
@@ -10,8 +10,16 @@ setup(
     use_scm_version={
         "root": ".",
         "relative_to": __file__,
-        "fallback_version": __FALLBACK_VERSION__},
-    setup_requires=['setuptools_scm>=3.5.0'],
-    packages=find_packages(),
-    install_requires=requirements
+        "fallback_version": __FALLBACK_VERSION__,
+    },
+    setup_requires=["setuptools_scm>=3.5.0"],
+    packages=find_packages(exclude=["tests"]),
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Unix",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+    ],
 )
