4b864e5f1bd7e4be892217e83c670a8687197cc2
Tarek Allam <t.allam.jr@gmail.com>
2022-02-08 15:00:50 +0000

    Runable tests with pytest and mpl
    	modified:   test_plots.py

--- astronet/tests/unit/viz/test_plots.py
+++ astronet/tests/unit/viz/test_plots.py
@@ -47,7 +47,7 @@ class TestPlots:
     """A class with common parameters, `architecture`, `dataset` and `model_name`."""
 
     @pytest.fixture
-    def fixt(self, architecture, dataset, model_name):
+    def fixt(self):
         """This fixture will only be available within the scope of TestPlots"""
         X_test = np.load(
             f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
@@ -60,18 +60,25 @@ class TestPlots:
         )
 
         inputs = [X_test, Z_test]
+
+        return X_test, y_test, Z_test, inputs
+
+    def compute_scores(self, architecture, dataset, model_name, fixt):
+
+        X_test, y_test, Z_test, inputs = fixt
+
         results_filename = (
             f"{asnwd}/astronet/{architecture}/models/{dataset}/results_with_z.json"
         )
 
         with open(results_filename) as f:
             events = json.load(f)
-            if self.model_name is not None:
+            if model_name is not None:
                 # Get params for model chosen with cli args
                 event = next(
                     item
                     for item in events["training_result"]
-                    if item["name"] == self.model_name
+                    if item["name"] == model_name
                 )
             else:
                 event = min(
@@ -86,17 +93,11 @@ class TestPlots:
         )
 
         dataform = "testset"
-        encoding, class_encoding, class_names = get_encoding(
-            self.dataset, dataform=dataform
-        )
+        encoding, class_encoding, class_names = get_encoding(dataset, dataform=dataform)
 
         y_preds = model.predict([X_test, Z_test])
 
         return (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
@@ -104,36 +105,36 @@ class TestPlots:
             y_preds,
         )
 
-    def test_one(self, architecture, dataset, model_name, fixt):
+    def test_params(self, architecture, dataset, model_name):
+        print("\ntest_one", architecture, dataset, model_name)
+
+    def test_fixtures(self, architecture, dataset, model_name, fixt):
         print("\ntest_one", architecture, dataset, model_name, fixt)
-        print(f"FILENAME: {fixt}")
 
     @pytest.mark.mpl_image_compare(hash_library="baseline/hashlib.json")
     def test_confusion_matrix(self, architecture, dataset, model_name, fixt):
 
         cmap = sns.light_palette("Navy", as_cmap=True)
 
+        X_test, y_test, Z_test, inputs = fixt
+
         (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
             model,
             y_preds,
-        ) = fixt
+        ) = self.compute_scores(architecture, dataset, model_name, fixt)
 
         fig = plot_confusion_matrix(
-            self.architecture,
-            self.dataset,
-            self.model_name,
+            architecture,
+            dataset,
+            model_name,
             y_test,
             y_preds,
             encoding,
             class_names,  # enc.categories_[0]
-            save=self.savefigs,
+            save=False,
             cmap=cmap,
         )
         return fig
@@ -141,17 +142,15 @@ class TestPlots:
     @pytest.mark.mpl_image_compare(hash_library="baseline/hashlib.json")
     def test_acc_history(self, architecture, dataset, model_name, fixt):
 
+        X_test, y_test, Z_test, inputs = fixt
+
         (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
             model,
             y_preds,
-        ) = fixt
+        ) = self.compute_scores(architecture, dataset, model_name, fixt)
 
         fig = plot_acc_history(
             architecture,
@@ -165,17 +164,15 @@ class TestPlots:
     @pytest.mark.mpl_image_compare(hash_library="baseline/hashlib.json")
     def test_loss_history(self, architecture, dataset, model_name, fixt):
 
+        X_test, y_test, Z_test, inputs = fixt
+
         (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
             model,
             y_preds,
-        ) = fixt
+        ) = self.compute_scores(architecture, dataset, model_name, fixt)
 
         fig = plot_loss_history(
             architecture,
@@ -189,22 +186,20 @@ class TestPlots:
     @pytest.mark.mpl_image_compare(hash_library="baseline/hashlib.json")
     def test_multiROC(self, architecture, dataset, model_name, fixt):
 
+        X_test, y_test, Z_test, inputs = fixt
+
         (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
             model,
             y_preds,
-        ) = fixt
+        ) = self.compute_scores(architecture, dataset, model_name, fixt)
 
         fig = plot_multiROC(
-            self.architecture,
-            self.dataset,
-            self.model_name,
+            architecture,
+            dataset,
+            model_name,
             model,
             inputs,
             y_test,
@@ -216,17 +211,15 @@ class TestPlots:
     @pytest.mark.mpl_image_compare(hash_library="baseline/hashlib.json")
     def test_multiPR(self, architecture, dataset, model_name, fixt):
 
+        X_test, y_test, Z_test, inputs = fixt
+
         (
-            X_test,
-            y_test,
-            Z_test,
-            inputs,
             event,
             encoding,
             class_names,
             model,
             y_preds,
-        ) = fixt
+        ) = self.compute_scores(architecture, dataset, model_name, fixt)
 
         fig = plot_multiPR(
             architecture,
