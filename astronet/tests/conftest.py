99e7f3bd2227a57325ae9ec3d8943a8435304488
Tarek Allam <t.allam.jr@gmail.com>
2022-02-11 14:55:12 +0000

    Use scope='session' for single load of fixt data
    Previously, using the default scope='function' meant test data was being
    load FOR EACH test case in 'test_plots.py' which may explain the 2hr
    turnaround time for the test case.
    
    This should hopefully speed up tests with a single 'conftest.py' file as
    well.
    
    Refs:
        - https://betterprogramming.pub/understand-5-scopes-of-pytest-fixtures-1b607b5c19ed
        - https://www.youtube.com/watch?v=Da-FL_1i6ps&t=1891s
    
    	new file:   astronet/tests/conftest.py
    	modified:   astronet/tests/unit/viz/test_plots.py
    	modified:   astronet/tests/unit/viz/test_visualise_data.ipynb

---
+++ astronet/tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+
+from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+
+
+@pytest.fixture
+def fixt(scope="session"):
+    """This fixture will only be available within the scope of TestPlots"""
+    X_test = np.load(
+        f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
+    )
+    y_test = np.load(
+        f"{asnwd}/data/plasticc/test_set/infer/y_test.npy",
+    )
+    Z_test = np.load(
+        f"{asnwd}/data/plasticc/test_set/infer/Z_test.npy",
+    )
+
+    inputs = [X_test, Z_test]
+
+    return X_test, y_test, Z_test, inputs
