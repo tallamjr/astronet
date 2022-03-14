dabb7d045e9654f335ead47d8e5e41899e693c78
Tarek Allam <t.allam.jr@gmail.com>
2022-03-14 07:47:05 +0000

    Add parameter count test for t2 model
    	modified:   tests/unit/t2/test_t2_model.py

---
+++ astronet/tests/unit/t2/test_t2_model.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pytest
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from astronet.t2.model import T2Model
+
+
+@pytest.mark.parametrize(
+    (
+        "droprate",
+        "embed_dim",
+        "ff_dim",
+        "num_heads",
+        "num_layers",
+        "num_aux_feats",
+        "answer",
+    ),
+    (
+        (0.1, 32, 128, 16, 1, 0, 13390),
+        (0.1, 32, 128, 16, 1, 2, 13390),
+    ),
+)
+def test_num_parameters(
+    droprate, embed_dim, ff_dim, num_heads, num_layers, num_aux_feats, answer
+):
+
+    # --> Number of filters to use in ConvEmbedding block, should be equal to embed_dim
+    num_filters = embed_dim
+
+    input_shape = (None, 100, 6)
+    model = T2Model(
+        input_shape,
+        embed_dim,
+        num_heads=num_heads,
+        ff_dim=ff_dim,
+        num_filters=num_filters,
+        num_classes=14,
+        num_layers=num_layers,
+        droprate=droprate,
+        num_aux_feats=num_aux_feats,
+        add_aux_feats_to="L",
+        # Either add features to M dimension or L dimension. Adding to L allows for
+        # visualisation of CAMs relating to redshift since we would have a CAM of (L + Z) x c
+        # fc_neurons=fc_neurons,
+    )
+    inputs = tf.keras.Input(shape=[100, 6])
+    model(inputs)
+    assert np.sum([K.count_params(p) for p in model.trainable_weights]) == answer
