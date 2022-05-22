32bf9d036849a25e5efcb10a2526bf8510fa0b74
Tarek Allam <t.allam.jr@gmail.com>
2022-05-22 18:22:30 +0100

    Add line profiling code for ztf alerts/fink broker
    model.predict() was the largest bottleneck, followed by robust_scale
    
    To speed up inference, once can pass inputs directly with model(input),
    or use TFLife for maximum speed up.
    
    There is some overhead with converting to TFLite model, but perhaps when
    running predictions over large number of alerts, or in a batched
    fashion, this might prove to be OK.
    
    Furthermore, loading a compressed clustered model is 3s faster than
    original model, so loading a compressed clustered model is worth the
    extract deflating regardless.
    
    Overall, options for improving latency:
      1. Load compressed clustered model + use normal model.predict()
        * Improved loading time from 7s --> 4s
      2. Load compressed clustered model + use model(inputs) instead of model.predict(inputs)
        * Improves prediction times from 0.4s to 0.3s
      3. Load compressed clustered model + convert model to TFLite model (LiteModel class), then do clmodel.predict()
        * Loading compressed model and converting adds overhead but still
          within that of loading normal model ~ 6s, and prediction time of
          0.1s
    
    Refs:
      - https://micwurm.medium.com/using-tensorflow-lite-to-speed-up-predictions-a3954886eb98
      - https://www.tensorflow.org/lite/api_docs/python/tf/lite/TFLiteConverter#from_saved_model
    
    NOTE:
      - An alternative is to look at multiprocessing the predict() function,
        but this seems to cause further complications
        Refs:
          - https://stackoverflow.com/q/60905801/4521950
          - https://stackoverflow.com/q/47726763/4521950
          - https://stackoverflow.com/q/8804830/4521950
    
    	new file:   astronet/t2/models/plasticc/tinho/compressed_clustered_stripped_fink_model/keras_metadata.pb
    	new file:   astronet/t2/models/plasticc/tinho/compressed_clustered_stripped_fink_model/saved_model.pb
    	new file:   astronet/t2/models/plasticc/tinho/compressed_clustered_stripped_fink_model/variables/variables.data-00000-of-00001
    	new file:   astronet/t2/models/plasticc/tinho/compressed_clustered_stripped_fink_model/variables/variables.index
    	new file:   sbin/lnprof/t2-mwe-ztf-clustered-tflite-model.lnprofile
    	new file:   sbin/lnprof/t2-mwe-ztf-compressed-clustered-tflite-model.lnprofile
    	new file:   sbin/lnprof/t2-mwe-ztf-compressed-model.lnprofile
    	new file:   sbin/lnprof/t2-mwe-ztf-original-model.lnprofile
    	new file:   sbin/t2-mwe-ztf.py

---
+++ sbin/t2-mwe-ztf.py
@@ -0,0 +1,374 @@
+import random
+import warnings
+import zipfile
+
+import pyspark.pandas as ps
+from fink_client.visualisation import extract_field
+
+import astronet
+from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+
+# %load -r 37-255 processor.py
+from astronet.preprocess import generate_gp_all_objects, robust_scale
+
+warnings.filterwarnings("ignore")
+
+import os
+from pathlib import Path
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from fink_science.conversion import mag2fluxcal_snana
+from tensorflow.python.ops.numpy_ops import np_config
+
+from astronet.metrics import WeightedLogLoss
+
+np_config.enable_numpy_behavior()
+
+
+class LiteModel:
+    @classmethod
+    def from_file(cls, model_path):
+        return LiteModel(tf.lite.Interpreter(model_path=model_path))
+
+    @classmethod
+    def from_saved_model(cls, model_path):
+        converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+        converter.target_spec.supported_ops = [
+            tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
+            tf.lite.OpsSet.SELECT_TF_OPS,  # enable TensorFlow ops.
+        ]
+        converter.experimental_enable_resource_variables = True
+        converter.experimental_new_converter = True
+        tflite_model = converter.convert()
+        return LiteModel(tf.lite.Interpreter(model_content=tflite_model))
+
+    def __init__(self, interpreter):
+        self.interpreter = interpreter
+        self.interpreter.allocate_tensors()
+        input_det = self.interpreter.get_input_details()[0]
+        output_det = self.interpreter.get_output_details()[0]
+        self.input_index = input_det["index"]
+        self.output_index = output_det["index"]
+        self.input_shape = input_det["shape"]
+        self.output_shape = output_det["shape"]
+        self.input_dtype = input_det["dtype"]
+        self.output_dtype = output_det["dtype"]
+
+    def predict(self, inp):
+        inp = inp.astype(self.input_dtype)
+        count = inp.shape[0]
+        out = np.zeros((count, self.output_shape[1]), dtype=self.output_dtype)
+        for i in range(count):
+            self.interpreter.set_tensor(self.input_index, inp[i : i + 1])
+            self.interpreter.invoke()
+            out[i] = self.interpreter.get_tensor(self.output_index)[0]
+        return out
+
+
+def extract_fields(alert: Dict):
+    # Get flux and error
+    magpsf = extract_field(alert, "magpsf")
+    sigmapsf = extract_field(alert, "sigmapsf")
+
+    jd = extract_field(alert, "jd")
+
+    # For rescaling dates to start at 0 --> 30
+    # dates = np.array([jd[0] - i for i in jd])
+
+    # FINK candidate ID (int64)
+    candid = alert["candid"]
+
+    # filter bands
+    fid = extract_field(alert, "fid")
+
+    return (magpsf, sigmapsf, jd, candid, fid)
+
+
+@profile
+def get_model(model_name: str = "model-23057-1642540624-0.1.dev963+g309c9d8"):
+    # Load pre-trained model
+    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+        compile=False,
+    )
+
+    return model
+
+
+@profile
+def get_compressed_lite_model(model_name: str = "23057-1642540624-0.1.dev963+g309c9d8"):
+    # Load pre-trained model
+    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+
+    with zipfile.ZipFile(f"{model_path}.zip", mode="r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, model_path)
+
+    lmodel = LiteModel.from_saved_model(model_path)
+
+    return lmodel
+
+
+@profile
+def get_compressed_model(model_name: str = "23057-1642540624-0.1.dev963+g309c9d8"):
+    # Load pre-trained model
+    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+
+    with zipfile.ZipFile(f"{model_path}.zip", mode="r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, model_path)
+
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+        compile=False,
+    )
+    return model
+
+
+@profile
+def t2_probs(
+    candid: np.int64,
+    jd: np.ndarray,
+    fid: np.ndarray,
+    magpsf: np.int64,
+    sigmapsf: np.int64,
+    model: tf.keras.Model,
+    prettyprint=None,
+) -> Dict:
+    """Compute probabilities of alerts in relation to PLAsTiCC classes using the Time-Series
+    Transformer model
+
+    Parameters
+    ----------
+    candid: np.int64,
+        Candidate IDs
+    jd: np.ndarray,
+        JD times (float)
+    fid: np.ndarray,
+        Filter IDs (int)
+    magpsf: np.ndarray,
+        Magnitude from PSF-fit photometry
+    sigmapsf: np.ndarray,
+        1-sigma error of PSF-fit
+    model: tensorflow.python.keras.saving.saved_model.load.T2Model
+        Pre-compiled T2 model
+
+    Returns
+    ----------
+    probabilities: dict
+        Dict containing np.array of float probabilities
+
+    Examples
+    ----------
+    >>> import pyspark.pandas as ps
+    >>> psdf = ps.read_parquet('sample.parquet')
+    >>> import random
+    >>> r = random.randint(0,len(psdf))
+    >>> alert = psdf.iloc[r]
+    >>> print(alert.head())
+    candid                                     1786552611115010001
+    schemavsn                                                  3.3
+    publisher                                                 Fink
+    objectId                                          ZTF18aaqfhlj
+    candidate    (2459541.0526157, 2, 1786552611115, 19.1966800...
+    Name: 221, dtype: object
+    >>> alert = alert.to_dict()
+
+    >>> from fink_client.visualisation import extract_field
+    # Get flux and error
+    >>> magpsf = extract_field(alert, 'magpsf')
+    >>> sigmapsf = extract_field(alert, 'sigmapsf')
+
+    >>> jd = extract_field(alert, "jd")
+
+    # For rescaling dates to start at 0 --> 30
+    # dates = np.array([jd[0] - i for i in jd])
+
+    # FINK candidate ID (int64)
+    >>> candid = alert["candid"]
+
+    # filter bands
+    >>> fid = extract_field(alert, "fid")
+
+    >>> model_name = "23057-1642540624-0.1.dev963+g309c9d8"
+    >>> model = get_model(model_name=model_name)
+
+    >>> t2_probs(candid, jd, fid, magpsf, sigmapsf, model)
+    {
+        "AGN": 0.0,
+        "EB": 0.017,
+        "KN": 0.0,
+        "M-dwarf": 0.891,
+        "Mira": 0.002,
+        "RRL": 0.004,
+        "SLSN-I": 0.0,
+        "SNII": 0.078,
+        "SNIa": 0.001,
+        "SNIa-91bg": 0.006,
+        "SNIax": 0.001,
+        "SNIbc": 0.001,
+        "TDE": 0.0,
+        "mu-Lens-Single": 0.0
+    }
+    """
+
+    ZTF_FILTER_MAP = {1: "ztfg", 2: "ztfr", 3: "ztfi"}
+
+    ZTF_PB_WAVELENGTHS = {
+        "ztfg": 4804.79,
+        "ztfr": 6436.92,
+        "ztfi": 7968.22,
+    }
+
+    # Rescale dates to _start_ at 0
+    dates = np.array([jd[0] - i for i in jd])
+
+    mjd, flux, flux_error, filters = ([] for i in range(4))
+
+    # Loop over each filter
+    filter_color = ZTF_FILTER_MAP
+    for filt in filter_color.keys():
+        mask = np.where(fid == filt)[0]
+
+        # Skip if no data
+        if len(mask) == 0:
+            continue
+
+        maskNotNone = magpsf[mask] != None
+        mjd.append(dates[mask][maskNotNone])
+        flux.append(magpsf[mask][maskNotNone])
+        flux_error.append(sigmapsf[mask][maskNotNone])
+        filters.append(filt)
+
+    df_tmp = pd.DataFrame.from_dict(
+        {
+            "mjd": mjd,
+            "object_id": candid,
+            "flux": flux,
+            "flux_error": flux_error,
+            "filters": filters,
+        }
+    )
+
+    df_tmp = df_tmp.apply(pd.Series.explode).reset_index()
+
+    # Re-compute flux and flux error
+    data = [
+        mag2fluxcal_snana(*args)
+        for args in zip(df_tmp["flux"].explode(), df_tmp["flux_error"].explode())
+    ]
+    flux, error = np.transpose(data)
+
+    # make a Pandas DataFrame with exploded series
+    pdf = pd.DataFrame.from_dict(
+        {
+            "filter": df_tmp["filters"].replace({1: "ztfg", 2: "ztfr"}),
+            "flux": flux,
+            "flux_error": error,
+            "mjd": df_tmp["mjd"],
+            "object_id": df_tmp["object_id"],
+        }
+    )
+
+    pdf = pdf.filter(["object_id", "mjd", "flux", "flux_error", "filter"])
+    # pdf = pdf.dropna()
+    # pdf = pdf.reset_index()
+
+    if not isinstance(candid, list):
+        object_list = [candid]
+    df_gp_mean = generate_gp_all_objects(
+        object_list, pdf, pb_wavelengths=ZTF_PB_WAVELENGTHS
+    )
+
+    cols = set(list(ZTF_PB_WAVELENGTHS.keys())) & set(df_gp_mean.columns)
+    robust_scale(df_gp_mean, cols)
+    X = df_gp_mean[cols]
+    X = np.asarray(X).astype("float32")
+    X = np.expand_dims(X, axis=0)
+
+    y_preds = model.predict(X)
+
+    class_names = [
+        "mu-Lens-Single",
+        "TDE",
+        "EB",
+        "SNII",
+        "SNIax",
+        "Mira",
+        "SNIbc",
+        "KN",
+        "M-dwarf",
+        "SNIa-91bg",
+        "AGN",
+        "SNIa",
+        "RRL",
+        "SLSN-I",
+    ]
+
+    keys = class_names
+    values = y_preds.tolist()
+    predictions = dict(zip(keys, values[0]))
+
+    if prettyprint is not None:
+        import json
+
+        print(
+            json.dumps(
+                json.loads(
+                    json.dumps(predictions), parse_float=lambda x: round(float(x), 3)
+                ),
+                indent=4,
+                sort_keys=True,
+            )
+        )
+
+    return predictions
+
+
+if __name__ == "__main__":
+
+    print(astronet.__version__)
+    print(astronet.__file__)
+
+    # psdf = ps.read_parquet(f"{asnwd}/data/ztf/alerts.parquet")
+    psdf = ps.read_parquet(f"{asnwd}/data/ztf/sample.parquet")
+
+    SEED = 2
+    random.seed(SEED)
+    r = random.randint(SEED, len(psdf))
+
+    print(r)
+    alert = psdf.iloc[r]
+    print(alert.head())
+    alert = alert.to_dict()
+
+    magpsf, sigmapsf, jd, candid, fid = extract_fields(alert)
+
+    # ORIGINAL MODEL
+    model = get_model()
+
+    # COMPRESSED CLUSTERED-STRIPPED MODEL
+    model_name = "tinho/compressed_clustered_stripped_fink_model"
+    cmodel = get_compressed_model(model_name)
+
+    # TFLITE COMPRESSED CLUSTERED-STRIPPED MODEL
+    clmodel = get_compressed_lite_model(model_name)
+
+    # TFLITE CLUSTERED-STRIPPED MODEL
+    model_name = "tinho/clustered_stripped_fink_model"
+    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+    lmodel = LiteModel.from_saved_model(model_path)
+
+    # t2_probs(candid, jd, fid, magpsf, sigmapsf, model=model, prettyprint=True)    # t2-mwe-ztf-original-model.lnprofile
+    # t2_probs(candid, jd, fid, magpsf, sigmapsf, model=cmodel, prettyprint=True)   # t2-mwe-ztf-compressed-model.lnprofile
+    # t2_probs(candid, jd, fid, magpsf, sigmapsf, model=lmodel, prettyprint=True)   # t2-mwe-ztf-clustered-tflite-model.lnprofile
+    t2_probs(
+        candid, jd, fid, magpsf, sigmapsf, model=clmodel, prettyprint=True
+    )  # t2-mwe-ztf-compressed-clustered-tflite-model.lnprofile
