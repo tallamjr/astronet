839715e9d8fb589148cfc9daea39f75379eaa9ba
Tarek Allam <t.allam.jr@gmail.com>
2022-05-26 00:09:07 +0100

    Return just the loss
    It may not be required to have extra steps defined:
    
        When used with tf.distribute.Strategy, outside of built-in training loops such
        as tf.keras compile and fit, please use 'SUM' or 'NONE' reduction types, and
        reduce losses explicitly in your training loop. Using 'AUTO' or
        'SUM_OVER_BATCH_SIZE' will raise an error.
    
    Since we run _inside_ fit, this may not be needed
    
    Refs:
        - https://www.tensorflow.org/api_docs/python/tf/keras/losses/Loss

--- astronet/metrics.py
+++ astronet/metrics.py
@@ -183,7 +183,8 @@ class DistributedWeightedLogLoss(keras.losses.Loss):
             )
         )
 
-        return tf.reduce_sum(loss * (1.0 / self.global_batch_size))
+        return loss
+        # return tf.reduce_sum(loss * (1.0 / self.global_batch_size))
 
         # return tf.nn.compute_average_loss(
         #     loss, global_batch_size=self.global_batch_size
