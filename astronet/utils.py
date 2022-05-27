f29b500a2eb31a3d6ce0dc8695c3f38eae5a2ccf
Tarek Allam <t.allam.jr@gmail.com>
2022-05-27 10:00:38 +0100

    OOM errors when batch too big, trialling 4096,8192
    	modified:   astronet/utils.py

--- astronet/utils.py
+++ astronet/utils.py
@@ -92,8 +92,7 @@ def find_optimal_batch_size(training_set_length: int) -> int:
         batch_size_list = [16, 32, 64]
     else:
         # batch_size_list = [96, 128, 256]
-        # batch_size_list = [1024, 2028, 4096]
-        batch_size_list = [524288, 786432]
+        batch_size_list = [4096, 8192]
     ratios = []
     for batch_size in batch_size_list:
 
