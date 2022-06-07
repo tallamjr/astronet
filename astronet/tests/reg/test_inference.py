2272524c0955985520a027bca9b0cdc1959d671a
Tarek Allam <t.allam.jr@gmail.com>
2022-06-07 11:01:27 +0100

    Place models and plots into correct dirs
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-acc-28341-1654269564-0.5.1.dev73+g70f85f8.pdf
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-cm-28341-1654269564-0.5.1.dev73+g70f85f8-CMB.pdf
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-cm-28341-1654269564-0.5.1.dev73+g70f85f8.pdf
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-loss-28341-1654269564-0.5.1.dev73+g70f85f8.pdf
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-pr-28341-1654269564-0.5.1.dev73+g70f85f8.pdf
    	deleted:    astronet/t2/plots/plasticc/28341-1654269564-0.5.1.dev73+g70f85f8/model-roc-28341-1654269564-0.5.1.dev73+g70f85f8.pdf
    	deleted:    astronet/t2/plots/plasticc/31367-1654360237-0.5.1.dev78+g702e399.d20220604/model-acc-31367-1654360237-0.5.1.dev78+g702e399.d20220604.pdf
    	deleted:    astronet/t2/plots/plasticc/31367-1654360237-0.5.1.dev78+g702e399.d20220604/model-cm-31367-1654360237-0.5.1.dev78+g702e399.d20220604.pdf
    	deleted:    astronet/t2/plots/plasticc/31367-1654360237-0.5.1.dev78+g702e399.d20220604/model-loss-31367-1654360237-0.5.1.dev78+g702e399.d20220604.pdf
    	deleted:    astronet/t2/plots/plasticc/31367-1654360237-0.5.1.dev78+g702e399.d20220604/model-pr-31367-1654360237-0.5.1.dev78+g702e399.d20220604.pdf
    	deleted:    astronet/t2/plots/plasticc/31367-1654360237-0.5.1.dev78+g702e399.d20220604/model-roc-31367-1654360237-0.5.1.dev78+g702e399.d20220604.pdf
    	new file:   astronet/tests/reg/test_inference.py
    	deleted:    astronet/tests/reg/test_model_load_and_predict.py
    	new file:   astronet/tinho/lite.py
    	deleted:    astronet/tinho/make_lite.py
    	new file:   astronet/tinho/models/plasticc/clustered_stripped_fink_model.tflite
    	new file:   astronet/tinho/models/plasticc/clustered_stripped_fink_model_quantized.tflite
    	renamed:    astronet/t2/models/plasticc/model-28341-1654269564-0.5.1.dev73+g70f85f8/keras_metadata.pb -> astronet/tinho/models/plasticc/model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836/keras_metadata.pb
    	renamed:    astronet/t2/models/plasticc/model-28341-1654269564-0.5.1.dev73+g70f85f8/saved_model.pb -> astronet/tinho/models/plasticc/model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836/saved_model.pb
    	renamed:    astronet/t2/models/plasticc/model-28341-1654269564-0.5.1.dev73+g70f85f8/variables/variables.data-00000-of-00001 -> astronet/tinho/models/plasticc/model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836/variables/variables.data-00000-of-00001
    	renamed:    astronet/t2/models/plasticc/model-28341-1654269564-0.5.1.dev73+g70f85f8/variables/variables.index -> astronet/tinho/models/plasticc/model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836/variables/variables.index
    	renamed:    astronet/tinho/models/plasticc/model-31367-1654360237-0.5.1.dev78+g702e399.d20220604/keras_metadata.pb -> astronet/tinho/models/plasticc/model-UGRIZY-31367-1654360237-0.5.1.dev78+g702e399.d20220604-LL0.450/keras_metadata.pb
    	renamed:    astronet/tinho/models/plasticc/model-31367-1654360237-0.5.1.dev78+g702e399.d20220604/saved_model.pb -> astronet/tinho/models/plasticc/model-UGRIZY-31367-1654360237-0.5.1.dev78+g702e399.d20220604-LL0.450/saved_model.pb
    	renamed:    astronet/tinho/models/plasticc/model-31367-1654360237-0.5.1.dev78+g702e399.d20220604/variables/variables.data-00000-of-00001 -> astronet/tinho/models/plasticc/model-UGRIZY-31367-1654360237-0.5.1.dev78+g702e399.d20220604-LL0.450/variables/variables.data-00000-of-00001
    	renamed:    astronet/tinho/models/plasticc/model-31367-1654360237-0.5.1.dev78+g702e399.d20220604/variables/variables.index -> astronet/tinho/models/plasticc/model-UGRIZY-31367-1654360237-0.5.1.dev78+g702e399.d20220604-LL0.450/variables/variables.index

---
+++ astronet/tests/reg/test_inference.py
@@ -0,0 +1,162 @@
+import os
+import random as python_random
+
+import numpy as np
+import pytest
+import tensorflow as tf
+from tensorflow import keras
+
+from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+from astronet.metrics import WeightedLogLoss
+from astronet.tinho.lite import LiteModel
+
+RANDOM_SEED = 42
+np.random.seed(RANDOM_SEED)
+tf.random.set_seed(RANDOM_SEED)
+
+# The below is necessary for starting core Python generated random numbers
+# in a well-defined state.
+python_random.seed(RANDOM_SEED)
+
+
+@pytest.mark.skipif(
+    os.getenv("CI") is not None, reason="Requires large data files. Run locally."
+)
+class TestInference:
+    """Test inference scores of the best performing models to date. This class is also a proxy for
+    inference timing of each model"""
+
+    @pytest.mark.parametrize(
+        ("architecture", "dataset", "model_name"),
+        (
+            ("atx", "plasticc", "9887359-1641295475-0.1.dev943+gc9bafac.d20220104"),
+            ("t2", "plasticc", "1619624444-0.1.dev765+g7c90cbb.d20210428"),
+        ),
+    )
+    def test_inference_with_z_numpy(
+        self, architecture, dataset, model_name, fixt_numpy
+    ):
+
+        # Previous models were trained using numpy data as the inputs, newer models leverage
+        # tf.data.Dataset instead for faster inference. This is a legacy requirment.
+        # Fix ValueError of shape mismatch.
+        X_test, y_test, Z_test, inputs = fixt_numpy
+
+        model = keras.models.load_model(
+            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+            compile=False,
+        )
+
+        wloss = WeightedLogLoss()
+        y_preds = model.predict(inputs)
+
+        if architecture == "atx":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.739, 0.01)
+        if architecture == "t2":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.507, 0.01)
+
+    @pytest.mark.parametrize(
+        ("architecture", "dataset", "model_name"),
+        (
+            (
+                "tinho",
+                "plasticc",
+                "UGRIZY-31367-1654360237-0.5.1.dev78+g702e399.d20220604-LL0.450",
+            ),
+        ),
+    )
+    def test_inference_with_z_tfdata(self, architecture, dataset, model_name, fixt):
+
+        test_ds, y_test_ds = fixt
+        y_test = np.concatenate([y for y in y_test_ds], axis=0)
+
+        model = keras.models.load_model(
+            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+            compile=False,
+        )
+
+        wloss = WeightedLogLoss()
+        y_preds = model.predict(test_ds)
+
+        if architecture == "tinho":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.450, 0.01)
+
+    @pytest.mark.parametrize(
+        ("architecture", "dataset", "model_name"),
+        (
+            ("tinho", "plasticc", "clustered_stripped_fink_model.tflite"),
+            (
+                "tinho-quantized",
+                "plasticc",
+                "clustered_stripped_fink_model_quantized.tflite",
+            ),
+        ),
+    )
+    def test_inference_no_z_tfdata_tflite(
+        self, architecture, dataset, model_name, fixtztf
+    ):
+
+        test_ds, y_test_ds = fixtztf
+        y_test = np.concatenate([y for y in y_test_ds], axis=0)
+        x_test = np.concatenate([x for x, y in test_ds], axis=0)
+
+        model_path = f"{asnwd}/astronet/tinho/models/{dataset}/{model_name}"
+        model = LiteModel.from_file(model_path=model_path)
+
+        wloss = WeightedLogLoss()
+        y_preds = model.predict(x_test)
+
+        if architecture == "tinho":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.95, 0.01)
+        if architecture == "tinho-quantized":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.94, 0.01)
+
+    @pytest.mark.parametrize(
+        ("architecture", "dataset", "model_name"),
+        (
+            ("atx", "plasticc", "206145-1644662345-0.3.1.dev36+gfd02ace"),
+            ("t2", "plasticc", "1619802068-0.1.dev779+ga930d1d"),
+        ),
+    )
+    def test_inference_no_z(self, architecture, dataset, model_name, fixtnoz):
+
+        test_ds, y_test_ds = fixtnoz
+        y_test = np.concatenate([y for y in y_test_ds], axis=0)
+        x_test = np.concatenate([x for x, y in test_ds], axis=0)
+
+        model = keras.models.load_model(
+            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+            compile=False,
+        )
+
+        wloss = WeightedLogLoss()
+        y_preds = model.predict(x_test)
+
+        if architecture == "atx":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.969, 0.01)
+        if architecture == "t2":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.873, 0.01)
+
+    @pytest.mark.parametrize(
+        ("architecture", "dataset", "model_name"),
+        (("tinho", "plasticc", "GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836"),),
+    )
+    def test_inference_clustered_ztf(self, architecture, dataset, model_name, fixtztf):
+
+        test_ds, y_test_ds = fixtztf
+        y_test = np.concatenate([y for y in y_test_ds], axis=0)
+
+        model = keras.models.load_model(
+            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+            compile=False,
+        )
+
+        wloss = WeightedLogLoss()
+        y_preds = model.predict(test_ds)
+
+        if architecture == "tinho":
+            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.836, 0.01)
---
+++ astronet/tinho/lite.py
@@ -0,0 +1,117 @@
+import os
+import random
+import subprocess
+import warnings
+import zipfile
+from pathlib import Path
+from typing import Dict
+
+import numpy as np
+import pandas as pd
+import pyspark.pandas as ps
+import tensorflow as tf
+from fink_client.visualisation import extract_field
+from fink_science.conversion import mag2fluxcal_snana
+from sklearn.preprocessing import robust_scale as rs
+from tensorflow.python.ops.numpy_ops import np_config
+
+import astronet
+from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+from astronet.metrics import WeightedLogLoss
+from astronet.preprocess import generate_gp_all_objects, robust_scale
+
+warnings.filterwarnings("ignore")
+np_config.enable_numpy_behavior()
+
+
+class LiteModel:
+    @classmethod
+    def from_file(cls, model_path):
+        return LiteModel(tf.lite.Interpreter(model_path=model_path))
+
+    @classmethod
+    def from_saved_model(cls, model_path, tflite_file_path=None):
+        converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+        converter.target_spec.supported_ops = [
+            tf.lite.OpsSet.TFLITE_BUILTINS,  # enable TensorFlow Lite ops.
+            tf.lite.OpsSet.SELECT_TF_OPS,  # enable TensorFlow ops.
+        ]
+        converter.experimental_enable_resource_variables = True
+        converter.experimental_new_converter = True
+        converter.optimizations = [tf.lite.Optimize.DEFAULT]
+        tflite_model = converter.convert()
+
+        if tflite_file_path is not None:
+            with open(tflite_file_path, "wb") as f:
+                f.write(tflite_model)
+
+        return LiteModel(tf.lite.Interpreter(model_content=tflite_model))
+
+    def __init__(self, interpreter):
+        self.interpreter = interpreter
+        self.interpreter.allocate_tensors()
+        input_det = self.interpreter.get_input_details()[0]
+        output_det = self.interpreter.get_output_details()[0]
+        self.input_index = input_det["index"]
+        self.output_index = output_det["index"]
+        self.input_shape = input_det["shape"]
+        self.output_shape = output_det["shape"]
+        self.input_dtype = input_det["dtype"]
+        self.output_dtype = output_det["dtype"]
+
+    def predict(self, inp):
+        inp = inp.astype(self.input_dtype)
+        count = inp.shape[0]
+        out = np.zeros((count, self.output_shape[1]), dtype=self.output_dtype)
+        for i in range(count):
+            self.interpreter.set_tensor(self.input_index, inp[i : i + 1])
+            self.interpreter.invoke()
+            out[i] = self.interpreter.get_tensor(self.output_index)[0]
+        return out
+
+
+def get_lite_model(
+    model_name: str = "23057-1642540624-0.1.dev963+g309c9d8", tflite_file_path=None
+):
+    # Load clustered model TFLite model, i.e. a .tflife model/file on disk
+    model_path = f"{asnwd}/sbin/lnprof/clustered_stripped_fink_model.tflite"
+    clmodel = LiteModel.from_file(model_path=model_path)
+
+    return clmodel
+
+
+def get_compressed_lite_model(
+    model_name: str = "23057-1642540624-0.1.dev963+g309c9d8", tflite_file_path=None
+):
+    # Load compressed clustered model TFLite model, i.e. was .tflife model/file but saved as .zip
+    # file on disk
+    model_path = f"{asnwd}/sbin/lnprof/__clustered_stripped_fink_model.tflite"
+
+    with zipfile.ZipFile(f"{model_path}.zip", mode="r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, model_path)
+
+    cclmodel = LiteModel.from_file(
+        model_path=f"{model_path}/clustered_stripped_fink_model.tflite"
+    )
+
+    return cclmodel
+
+
+def get_clustered_model_to_lite(
+    tflite_file_path="clustered_stripped_fink_model_quantized.tflite",
+):
+
+    model_path = (
+        f"{asnwd}/astronet/t2/models/plasticc/tinho/clustered_stripped_fink_model"
+    )
+    c2lmodel = LiteModel.from_saved_model(model_path, tflite_file_path=tflite_file_path)
+
+    return c2lmodel
+
+
+if __name__ == "__main__":
+
+    model = get_lite_model()
+    model = get_compressed_lite_model()
+    model = get_clustered_model_to_lite()
