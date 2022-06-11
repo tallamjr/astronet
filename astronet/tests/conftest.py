bc54ca70d5c762def403a93d3768717214c3201a
Tarek Allam <t.allam.jr@gmail.com>
2022-06-11 17:49:25 +0100

    [WIP] Attempt of using code example
    Problems come since we are returning a tuple of items with the fixtures,
    as well as the issue that TFDataset is not serializable via JSON
    
    _tbc_
    
    	modified:   astronet/tests/conftest.py
    	modified:   astronet/tests/reg/test_inference.py
    	modified:   astronet/tests/reg/test_plots.py

--- astronet/tests/conftest.py
+++ astronet/tests/conftest.py
@@ -1,8 +1,10 @@
+import json
 import subprocess
 
 import numpy as np
 import pytest
 import tensorflow as tf
+from filelock import FileLock
 
 from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
 from astronet.constants import LOCAL_DEBUG
@@ -20,26 +22,38 @@ SKIP_IF_M1 = pytest.mark.skipif(ISA == "arm64", reason="Error on arm-m1")
 BATCH_SIZE = 64
 
 
-@pytest.fixture
-def fixt_UGRIZY_wZ_numpy(scope="session"):
-    """This fixture will only be available within the scope of TestPlots"""
-    X_test = np.load(
-        f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
-    )
-    y_test = np.load(
-        f"{asnwd}/data/plasticc/test_set/infer/y_test.npy",
-    )
-    Z_test = np.load(
-        f"{asnwd}/data/plasticc/test_set/infer/Z_test.npy",
-    )
+@pytest.fixture(scope="session")
+def get_fixt_UGRIZY_wZ(tmp_path_factory, worker_id, name="fixt_UGRIZY_wZ"):
+    if not worker_id:
+        # not executing in with multiple workers, just produce the data and let
+        # pytest's fixture caching do its job
+        return fixt_UGRIZY_wZ()
 
-    inputs = [X_test, Z_test]
+    # get the temp directory shared by all workers
+    root_tmp_dir = tmp_path_factory.getbasetemp().parent
 
-    return X_test, y_test, Z_test, inputs
+    fn = root_tmp_dir / "data.json"
+    with FileLock(str(fn) + ".lock"):
+        if fn.is_file():
+            tdata = json.loads(fn.read_text())
+            ldata = list(tdata)
+            for item, index in enumerate(ldata):
+                ldata[item] = tf.convert_to_tensor(index)
+            data = tuple(ldata)
+        else:
+            tdata = fixt_UGRIZY_wZ()
+            import pdb
+
+            pdb.set_trace()
+            ldata = list(tdata)
+            for item, index in enumerate(ldata):
+                ldata[item] = item.numpy()
+            data = tuple(ldata)
+            fn.write_text(json.dumps(data))
+    return data
 
 
-@pytest.fixture
-def fixt_UGRIZY_wZ(scope="session"):
+def fixt_UGRIZY_wZ():
     """This fixture will only be available within the scope of TestPlots"""
     X_test = np.load(
         f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
@@ -75,8 +89,27 @@ def fixt_UGRIZY_wZ(scope="session"):
     return test_ds, y_test_ds, test_input
 
 
-@pytest.fixture
-def fixt_UGRIZY_noZ(scope="session"):
+@pytest.fixture(scope="session")
+def get_fixt_UGRIZY_noZ(tmp_path_factory, worker_id, name="fixt_UGRIZY_noZ"):
+    if not worker_id:
+        # not executing in with multiple workers, just produce the data and let
+        # pytest's fixture caching do its job
+        return fixt_UGRIZY_noZ()
+
+    # get the temp directory shared by all workers
+    root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+    fn = root_tmp_dir / "data.json"
+    with FileLock(str(fn) + ".lock"):
+        if fn.is_file():
+            data = json.loads(fn.read_text())
+        else:
+            data = fixt_UGRIZY_noZ()
+            fn.write_text(json.dumps(data))
+    return data
+
+
+def fixt_UGRIZY_noZ():
     """This fixture will only be available within the scope of TestPlots"""
     X_test = np.load(
         f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
@@ -107,8 +140,27 @@ def fixt_UGRIZY_noZ(scope="session"):
     return test_ds, y_test_ds
 
 
-@pytest.fixture
-def fixt_GR_noZ(scope="session"):
+@pytest.fixture(scope="session")
+def get_fixt_GR_noZ(tmp_path_factory, worker_id, name="fixt_GR_noZ"):
+    if not worker_id:
+        # not executing in with multiple workers, just produce the data and let
+        # pytest's fixture caching do its job
+        return fixt_GR_noZ()
+
+    # get the temp directory shared by all workers
+    root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+    fn = root_tmp_dir / "data.json"
+    with FileLock(str(fn) + ".lock"):
+        if fn.is_file():
+            data = json.loads(fn.read_text())
+        else:
+            data = fixt_GR_noZ()
+            fn.write_text(json.dumps(data))
+    return data
+
+
+def fixt_GR_noZ():
     """This fixture will only be available within the scope of TestPlots"""
     X_test = np.load(
         f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
@@ -138,3 +190,21 @@ def fixt_GR_noZ(scope="session"):
         y_test_ds = y_test_ds.take(300)
 
     return test_ds, y_test_ds
+
+
+# @pytest.fixture
+# def fixt_UGRIZY_wZ_numpy(scope="session"):
+#     """This fixture will only be available within the scope of TestPlots"""
+#     X_test = np.load(
+#         f"{asnwd}/data/plasticc/test_set/infer/X_test.npy",
+#     )
+#     y_test = np.load(
+#         f"{asnwd}/data/plasticc/test_set/infer/y_test.npy",
+#     )
+#     Z_test = np.load(
+#         f"{asnwd}/data/plasticc/test_set/infer/Z_test.npy",
+#     )
+
+#     inputs = [X_test, Z_test]
+
+#     return X_test, y_test, Z_test, inputs
--- astronet/tests/reg/test_inference.py
+++ astronet/tests/reg/test_inference.py
@@ -50,13 +50,13 @@ class TestInference:
         ),
     )
     def test_inference_UGRIZY_wZ(
-        self, architecture, dataset, model_name, fixt_UGRIZY_wZ
+        self, architecture, dataset, model_name, get_fixt_UGRIZY_wZ
     ):
 
         # Previous models were trained using numpy data as the inputs, newer models leverage
         # tf.data.Dataset instead for faster inference. This is a legacy requirment.
         # Fix ValueError of shape mismatch.
-        test_ds, y_test_ds, test_inputs = fixt_UGRIZY_wZ
+        test_ds, y_test_ds, test_inputs = get_fixt_UGRIZY_wZ
 
         model = keras.models.load_model(
             f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
@@ -76,113 +76,113 @@ class TestInference:
         if architecture == "tinho":
             assert wloss(y_test, y_preds).numpy() == pytest.approx(0.450, 0.01)
 
-    @pytest.mark.parametrize(
-        ("architecture", "dataset", "model_name"),
-        (
-            # TODO: ("atx", "plasticc", "XXX"), --> Missing model ID. Need to reproduce 0.929 result
-            ("t2", "plasticc", "UGRIZY-noZ-1619802068-0.1.dev779+ga930d1d-LL0.873"),
-        ),
-    )
-    def test_inference_UGRIZY_noZ(
-        self, architecture, dataset, model_name, fixt_UGRIZY_noZ
-    ):
+    # @pytest.mark.parametrize(
+    #     ("architecture", "dataset", "model_name"),
+    #     (
+    #         # TODO: ("atx", "plasticc", "XXX"), --> Missing model ID. Need to reproduce 0.929 result
+    #         ("t2", "plasticc", "UGRIZY-noZ-1619802068-0.1.dev779+ga930d1d-LL0.873"),
+    #     ),
+    # )
+    # def test_inference_UGRIZY_noZ(
+    #     self, architecture, dataset, model_name, get_fixt_UGRIZY_noZ
+    # ):
 
-        test_ds, y_test_ds = fixt_UGRIZY_noZ
-        y_test = np.concatenate([y for y in y_test_ds], axis=0)
-        x_test = np.concatenate([x for x, y in test_ds], axis=0)
+    #     test_ds, y_test_ds = get_fixt_UGRIZY_noZ
+    #     y_test = np.concatenate([y for y in y_test_ds], axis=0)
+    #     x_test = np.concatenate([x for x, y in test_ds], axis=0)
 
-        model = keras.models.load_model(
-            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
-            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
-            compile=False,
-        )
+    #     model = keras.models.load_model(
+    #         f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+    #         custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+    #         compile=False,
+    #     )
 
-        wloss = WeightedLogLoss()
-        y_preds = model.predict(x_test)
+    #     wloss = WeightedLogLoss()
+    #     y_preds = model.predict(x_test)
 
-        if architecture == "atx":
-            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.969, 0.01)
-        if architecture == "t2":
-            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.873, 0.01)
+    #     if architecture == "atx":
+    #         assert wloss(y_test, y_preds).numpy() == pytest.approx(0.969, 0.01)
+    #     if architecture == "t2":
+    #         assert wloss(y_test, y_preds).numpy() == pytest.approx(0.873, 0.01)
 
-    @pytest.mark.parametrize(
-        ("architecture", "dataset", "model_name"),
-        (
-            (
-                "atx",
-                "plasticc",
-                "GR-noZ-206145-1644662345-0.3.1.dev36+gfd02ace-LL0.969",
-            ),
-            (
-                "t2",
-                "plasticc",
-                "GR-noZ-23057-1642540624-0.1.dev963+g309c9d8-LL0.968",
-            ),
-            (
-                "tinho",
-                "plasticc",
-                "GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836",
-            ),
-        ),
-    )
-    def test_inference_GR_noZ(self, architecture, dataset, model_name, fixt_GR_noZ):
+    # @pytest.mark.parametrize(
+    #     ("architecture", "dataset", "model_name"),
+    #     (
+    #         (
+    #             "atx",
+    #             "plasticc",
+    #             "GR-noZ-206145-1644662345-0.3.1.dev36+gfd02ace-LL0.969",
+    #         ),
+    #         (
+    #             "t2",
+    #             "plasticc",
+    #             "GR-noZ-23057-1642540624-0.1.dev963+g309c9d8-LL0.968",
+    #         ),
+    #         (
+    #             "tinho",
+    #             "plasticc",
+    #             "GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836",
+    #         ),
+    #     ),
+    # )
+    # def test_inference_GR_noZ(self, architecture, dataset, model_name, get_fixt_GR_noZ):
 
-        test_ds, y_test_ds = fixt_GR_noZ
-        y_test = np.concatenate([y for y in y_test_ds], axis=0)
+    #     test_ds, y_test_ds = get_fixt_GR_noZ
+    #     y_test = np.concatenate([y for y in y_test_ds], axis=0)
 
-        model = keras.models.load_model(
-            f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
-            custom_objects={"WeightedLogLoss": WeightedLogLoss()},
-            compile=False,
-        )
+    #     model = keras.models.load_model(
+    #         f"{asnwd}/astronet/{architecture}/models/{dataset}/model-{model_name}",
+    #         custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+    #         compile=False,
+    #     )
 
-        wloss = WeightedLogLoss()
-        y_preds = model.predict(test_ds)
+    #     wloss = WeightedLogLoss()
+    #     y_preds = model.predict(test_ds)
 
-        if architecture == "atx":
-            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.969, 0.01)
-        if architecture == "t2":
-            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.968, 0.01)
-        if architecture == "tinho":
-            assert wloss(y_test, y_preds).numpy() == pytest.approx(0.836, 0.01)
+    #     if architecture == "atx":
+    #         assert wloss(y_test, y_preds).numpy() == pytest.approx(0.969, 0.01)
+    #     if architecture == "t2":
+    #         assert wloss(y_test, y_preds).numpy() == pytest.approx(0.968, 0.01)
+    #     if architecture == "tinho":
+    #         assert wloss(y_test, y_preds).numpy() == pytest.approx(0.836, 0.01)
 
-    @pytest.mark.parametrize(
-        ("architecture", "dataset", "model_name"),
-        (
-            (
-                "tinho",
-                "plasticc",
-                "model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite",
-            ),
-            (
-                "tinho-quantized",
-                "plasticc",
-                "quantized-model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite",
-            ),
-        ),
-    )
-    def test_inference_GR_noZ_TFLITE(
-        self, architecture, dataset, model_name, fixt_GR_noZ
-    ):
+    # @pytest.mark.parametrize(
+    #     ("architecture", "dataset", "model_name"),
+    #     (
+    #         (
+    #             "tinho",
+    #             "plasticc",
+    #             "model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite",
+    #         ),
+    #         (
+    #             "tinho-quantized",
+    #             "plasticc",
+    #             "quantized-model-GR-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite",
+    #         ),
+    #     ),
+    # )
+    # def test_inference_GR_noZ_TFLITE(
+    #     self, architecture, dataset, model_name, get_fixt_GR_noZ
+    # ):
 
-        test_ds, y_test_ds = fixt_GR_noZ
-        y_test = np.concatenate([y for y in y_test_ds], axis=0)
-        x_test = np.concatenate([x for x, y in test_ds], axis=0)
+    #     test_ds, y_test_ds = get_fixt_GR_noZ
+    #     y_test = np.concatenate([y for y in y_test_ds], axis=0)
+    #     x_test = np.concatenate([x for x, y in test_ds], axis=0)
 
-        model_path = f"{asnwd}/astronet/tinho/models/{dataset}/{model_name}"
-        model = LiteModel.from_file(model_path=model_path)
+    #     model_path = f"{asnwd}/astronet/tinho/models/{dataset}/{model_name}"
+    #     model = LiteModel.from_file(model_path=model_path)
 
-        wloss = WeightedLogLoss()
-        y_preds = model.predict(x_test)
+    #     wloss = WeightedLogLoss()
+    #     y_preds = model.predict(x_test)
 
-        if architecture == "tinho":
-            loss = wloss(y_test, y_preds).numpy()
-            log.info(f"LOSS tinho: {loss:.3f}")
-            assert loss == pytest.approx(0.836, 0.001)
-        if architecture == "tinho-quantized":
-            loss = wloss(y_test, y_preds).numpy()
-            log.info(f"LOSS tinho-quantized: {loss:.3f}")
-            assert loss == pytest.approx(0.834, 0.001)
+    #     if architecture == "tinho":
+    #         loss = wloss(y_test, y_preds).numpy()
+    #         log.info(f"LOSS tinho: {loss:.3f}")
+    #         assert loss == pytest.approx(0.836, 0.001)
+    #     if architecture == "tinho-quantized":
+    #         loss = wloss(y_test, y_preds).numpy()
+    #         log.info(f"LOSS tinho-quantized: {loss:.3f}")
+    #         assert loss == pytest.approx(0.834, 0.001)
 
     # @pytest.mark.parametrize(
     #     ("architecture", "dataset", "model_name"),
