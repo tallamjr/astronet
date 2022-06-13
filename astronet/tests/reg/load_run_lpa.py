e6681d80c2948850d2e999cb2263235549aee8db
Tarek Allam <t.allam.jr@gmail.com>
2022-06-13 15:45:13 +0100

    Update version of isort: 5.10.1
    	modified:   ../../../.pre-commit-config.yaml
    	modified:   load_run_lpa.py

--- astronet/tests/reg/load_run_lpa.py
+++ astronet/tests/reg/load_run_lpa.py
@@ -1,26 +1,29 @@
-import random
+import inspect
 import warnings
 import zipfile
 
-from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.ops.numpy_ops import np_config
 
 import astronet
+from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
+from astronet.metrics import WeightedLogLoss
+from astronet.tinho.lite import LiteModel
+from astronet.utils import astronet_logger
+
+log = astronet_logger(__file__)
 
 warnings.filterwarnings("ignore")
 
-import numpy as np
-import tensorflow as tf
-from astronet.metrics import WeightedLogLoss
-from astronet.tinho.lite import LiteModel
-from tensorflow.python.ops.numpy_ops import np_config
-
 np_config.enable_numpy_behavior()
+# flake8: noqa: C901
 
 
 def get_model(
     model_name: str = "model-GR-noZ-23057-1642540624-0.1.dev963+g309c9d8-LL0.968",
 ):
-    # Load pre-trained model
+    # Load pre-trained original t2 model
     model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
 
     model = tf.keras.models.load_model(
@@ -32,7 +35,27 @@ def get_model(
     return model
 
 
-def get_compressed_lite_model(model_name: str = "23057-1642540624-0.1.dev963+g309c9d8"):
+def get_compressed_model(
+    model_name: str = "model-GR-noZ-23057-1642540624-0.1.dev963+g309c9d8-LL0.968",
+):
+    # Load pre-trained zipped original t2 model
+    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+
+    with zipfile.ZipFile(f"{model_path}.zip", mode="r") as archive:
+        for file in archive.namelist():
+            archive.extract(file, model_path)
+
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+        compile=False,
+    )
+    return model
+
+
+def get_compressed_convert_to_lite(
+    model_name: str = "model-GR-noZ-23057-1642540624-0.1.dev963+g309c9d8-LL0.968",
+):
     # Load pre-trained model
     model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
 
@@ -45,9 +68,26 @@ def get_compressed_lite_model(model_name: str = "23057-1642540624-0.1.dev963+g30
     return lmodel
 
 
-def get_compressed_model(model_name: str = "23057-1642540624-0.1.dev963+g309c9d8"):
+def get_clustered_model(
+    model_name: str = "model-GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836",
+):
+    # Load pre-trained original t2 model
+    model_path = f"{asnwd}/astronet/tinho/models/plasticc/{model_name}"
+
+    model = tf.keras.models.load_model(
+        model_path,
+        custom_objects={"WeightedLogLoss": WeightedLogLoss()},
+        compile=False,
+    )
+
+    return model
+
+
+def get_compressed_clustered_model(
+    model_name: str = "model-GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836",
+):
     # Load pre-trained model
-    model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
+    model_path = f"{asnwd}/astronet/tinho/models/plasticc/{model_name}"
 
     with zipfile.ZipFile(f"{model_path}.zip", mode="r") as archive:
         for file in archive.namelist():
@@ -61,29 +101,86 @@ def get_compressed_model(model_name: str = "23057-1642540624-0.1.dev963+g309c9d8
     return model
 
 
+def get_pruned_model():
+    log.critical(f"{inspect.stack()[0].function} -- Not Fully Implemented Yet")
+    pass
+
+
+def get_compressed_clustered_pruned_model():
+    log.critical(f"{inspect.stack()[0].function} -- Not Fully Implemented Yet")
+    pass
+
+
 @profile
 def predict_original_model(X_test, wloss):
-    # ORIGINAL MODEL
+    # ORIGINAL T2 MODEL ON GR-noZ
+    # BASELINE
     model = get_model()
     y_preds = model.predict(X_test)
-    print(f"ORIGINAL MODEL LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
+    print(f"ORIGINAL T2 MODEL ON GR-noZ LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
+
+
+@profile
+def predict_compressed_model(X_test, wloss):
+    # COMPRESSED MODEL, aka COMPRESSED T2
+    # BASELINE + HUFFMAN
+    cmodel = get_compressed_model()
+    y_preds = cmodel.predict(X_test)
+    print(
+        f"COMPRESSED MODEL, aka COMPRESSED T2 LL-Test: {wloss(y_test, y_preds).numpy():.2f}"
+    )
+
+
+@profile
+def predict_clustered_model(X_test, wloss):
+    # CLUSTERED-STRIPPED MODEL, aka TINHO
+    # CLUSTERING
+    cmodel = get_clustered_model()
+    y_preds = cmodel.predict(X_test)
+    print(
+        f"CLUSTERED-STRIPPED MODEL, aka TINHO LL-Test: {wloss(y_test, y_preds).numpy():.2f}"
+    )
 
 
 @profile
 def predict_compressed_clustered_model(X_test, wloss):
-    # COMPRESSED CLUSTERED-STRIPPED MODEL
-    model_name = "tinho/compressed_clustered_stripped_fink_model"
-    cmodel = get_compressed_model(model_name)
+    # COMPRESSED CLUSTERED-STRIPPED MODEL, aka COMPRESSED TINHO
+    # CLUSTERING + HUFFMAN
+    cmodel = get_compressed_clustered_model()
     y_preds = cmodel.predict(X_test)
     print(
-        f"COMPRESSED CLUSTERED-STRIPPED MODEL LL-Test: {wloss(y_test, y_preds).numpy():.2f}"
+        f"COMPRESSED CLUSTERED-STRIPPED MODEL, aka COMPRESSED TINHO LL-Test: {wloss(y_test, y_preds).numpy():.2f}"
+    )
+
+
+@profile
+def predict_pruned_model(X_test, wloss):
+    # PRUNED-STRIPPED MODEL
+    # PRUNING
+    # TODO: Update model_name
+    model_name = ""
+    pmodel = get_pruned_model(model_name)
+    y_preds = pmodel.predict(X_test)
+    print(f"PRUNED-STRIPPED MODEL, LL-Test: {wloss(y_test, y_preds).numpy():.2f}")
+
+
+@profile
+def predict_compressed_clustered_pruned_model(X_test, wloss):
+    # COMPRESSED CLUSTERED-PRUNED-STRIPPED MODEL, aka COMPRESSED TINHO
+    # CLUSTERING + PRUNING + HUFFMAN
+    # TODO: Update model_name
+    model_name = ""
+    ccpmodel = get_compressed_clustered_pruned_model(model_name)
+    y_preds = ccpmodel.predict(X_test)
+    print(
+        f"COMPRESSED CLUSTERED-PRUNED-STRIPPED MODEL, LL-Test: {wloss(y_test, y_preds).numpy():.2f}"
     )
 
 
 @profile
 def predict_clustered_tflite_model(X_test, wloss):
-    # TFLITE CLUSTERED-STRIPPED MODEL
-    model_name = "tinho/clustered_stripped_fink_model"
+    # CLUSTERED-STRIPPED MODEL (TINHO), TFLITE INTERPRETER
+    # CLUSTERING-FLATBUFFER CONVERSION
     model_path = f"{asnwd}/astronet/t2/models/plasticc/{model_name}"
     lmodel = LiteModel.from_saved_model(model_path)
     y_preds = lmodel.predict(X_test)
@@ -94,7 +191,9 @@ def predict_clustered_tflite_model(X_test, wloss):
 
 @profile
 def predict_compressed_clustered_tflite_model(X_test, wloss):
-    # TFLITE COMPRESSED CLUSTERED-STRIPPED MODEL
+    # COMPRESSED CLUSTERED-STRIPPED MODEL (TINHO), TFLITE INTERPRETER
+    # CLUSTERING-FLATBUFFER + HUFFMAN
+    # TODO: Update model_name
     model_name = "tinho/compressed_clustered_stripped_fink_model"
     clmodel = get_compressed_lite_model(model_name)
     y_preds = clmodel.predict(X_test)
@@ -105,9 +204,10 @@ def predict_compressed_clustered_tflite_model(X_test, wloss):
 
 @profile
 def predict_saved_clustered_tflite_model(X_test, wloss):
-    # SAVED TFLITE COMPRESSED CLUSTERED-STRIPPED MODEL
+    # SAVED TFLITE CLUSTERED-STRIPPED MODEL, .tflife FILE
+    # CLUSTERING-FLATBUFFER
     # Load clustered model TFLite model, i.e. a .tflife model/file on disk
-    model_path = f"{asnwd}/sbin/lnprof/clustered_stripped_fink_model.tflite"
+    model_path = f"{asnwd}/tinho/models/plasticc/model-GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite"
     clmodel = LiteModel.from_file(model_path=model_path)
     y_preds = clmodel.predict(X_test)
     print(
@@ -117,10 +217,11 @@ def predict_saved_clustered_tflite_model(X_test, wloss):
 
 @profile
 def predict_saved_clustered_quantized_tflite_model(X_test, wloss):
-    # SAVED TFLITE COMPRESSED CLUSTERED-STRIPPED MODEL
+    # SAVED QUANTIZED TFLITE CLUSTERED-STRIPPED MODEL, .tflife FILE
+    # CLUSTERING-FLATBUFFER + QUANTIZATION
     # Load clustered model TFLite model, i.e. a .tflife model/file on disk
     # model_path = f"{asnwd}/sbin/lnprof/clustered_stripped_fink_model_quantized.tflite"
-    model_path = f"{asnwd}/sbin/lnprof/quantized.tflite"
+    model_path = f"{asnwd}/tinho/models/plasticc/quantized-model-GR-noZ-28341-1654269564-0.5.1.dev73+g70f85f8-LL0.836.tflite"
     clmodel = LiteModel.from_file(model_path=model_path)
     y_preds = clmodel.predict(X_test)
     print(
@@ -152,11 +253,20 @@ if __name__ == "__main__":
     print("Running predictions")
     wloss = WeightedLogLoss()
 
+    # BASELINE
     predict_original_model(X_test, wloss)
-    predict_clustered_tflite_model(X_test, wloss)
+    # BASELINE + HUFFMAN
+    predict_compressed_model(X_test, wloss)
 
-    predict_saved_clustered_tflite_model(X_test, wloss)
-    predict_saved_clustered_quantized_tflite_model(X_test, wloss)
-
-    predict_compressed_clustered_tflite_model(X_test, wloss)
+    # CLUSTERING
+    predict_clustered_model(X_test, wloss)
+    # CLUSTERING + HUFFMAN
     predict_compressed_clustered_model(X_test, wloss)
+
+    # TODO: CLUSTERING + PRUNING
+    # TODO: CLUSTERING + PRUNING + HUFFMAN
+
+    # CLUSTERING-FLATBUFFER
+    predict_saved_clustered_tflite_model(X_test, wloss)
+    # CLUSTERING-FLATBUFFER + QUANTIZATION
+    predict_saved_clustered_quantized_tflite_model(X_test, wloss)
