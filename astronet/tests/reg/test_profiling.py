d6bdbebf2e16fe141c87cfbeea68d3d6e2224c37
Tarek Allam <t.allam.jr@gmail.com>
2022-06-14 00:23:22 +0100

    Add results from line profiling tests
    	modified:   load_run_lpa.py.lprof
    	modified:   load_run_lpa.py.stdout.txt
    	modified:   test_profiling.py

--- astronet/tests/reg/test_profiling.py
+++ astronet/tests/reg/test_profiling.py
@@ -14,20 +14,22 @@ log = astronet_logger(__file__)
 def test_kernprof():
     prof = "load_run_lpa.py"
     _ = subprocess.run(
-        f"kernprof -lv {prof}",
+        f"kernprof -lv {prof} 2>&1 | tee {prof}.stdout.txt",
         check=True,
         capture_output=True,
         shell=True,
+        executable="/opt/homebrew/bin/bash",
         text=True,
     ).stdout.strip()
 
     log.info(f"KERNPROF DONE: Saved to {prof}.lnprof")
 
     out = subprocess.run(
-        f"python -m line_profiler {prof}.lprof 2>>&1 | tee {prof}.stdout.txt",
+        f"python -m line_profiler {prof}.lprof &>> {prof}.stdout.txt",
         check=True,
         capture_output=True,
         shell=True,
+        executable="/opt/homebrew/bin/bash",
         text=True,
     ).stdout.strip()
 
