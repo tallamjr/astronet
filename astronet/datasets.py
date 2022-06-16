07f6a65101435f5a6446d0c436f93d1d9074542f
Tarek Allam <t.allam.jr@gmail.com>
2022-06-16 11:00:19 +0100

    New function to lazy-load large numpy into tf.data
    Refs:
      - https://stackoverflow.com/a/58676345/4521950
    
    	modified:   datasets.py

--- astronet/datasets.py
+++ astronet/datasets.py
@@ -1,33 +1,10 @@
-import logging
-import os
-import pickle
-from pathlib import Path
-from typing import Dict, List
+import random as python_random
 
-import joblib
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from scipy import stats
-from sklearn import model_selection
-from sklearn.preprocessing import OneHotEncoder
 
-from astronet.constants import ASTRONET_WORKING_DIRECTORY as asnwd
-from astronet.constants import (
-    LSST_FILTER_MAP,
-    LSST_PB_WAVELENGTHS,
-    PLASTICC_CLASS_MAPPING,
-    SYSTEM,
-)
-from astronet.metrics import WeightedLogLoss
-from astronet.preprocess import (
-    __filter_dataframe_only_supernova,
-    __transient_trim,
-    generate_gp_all_objects,
-    one_hot_encode,
-    remap_filters,
-    robust_scale,
-)
+from astronet.utils import astronet_logger
 
 log = astronet_logger(__file__)
 
@@ -43,8 +20,7 @@ python_random.seed(RANDOM_SEED)
 pd.options.mode.chained_assignment = None  # default='warn'
 
 
-def tfdataset_from_numpy(dataset: str = "plasticc"):
-    log.critical(f"{inspect.stack()[0].function} -- Not Fully Implemented Yet")
+def lazy_load_tfdataset_from_numpy(file: str):
 
     # memmap the file
     numpy_data_memmap = np.load(file, mmap_mode="r")
@@ -56,8 +32,11 @@ def tfdataset_from_numpy(dataset: str = "plasticc"):
     # create tf dataset from generator fn
     dataset = tf.data.Dataset.from_generator(
         generator=data_generator,
-        output_types=np.float64,
-        output_shapes=example_shape,
+        output_signature=tf.type_spec_from_value(
+            numpy_data_memmap[0]
+        ),  # Infer spec from first element
+        # output_types=numpy_data_memmap.dtype,  DEPRECATED
+        # output_shapes=numpy_data_memmap.shape[1:],  # shape, no batch DEPRECATED
     )
 
     return dataset
