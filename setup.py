0380978bbff241b4f0afa3f1b24ac1a31b89799c
Tarek Allam Jr <t.allam.jr@gmail.com>
2022-06-29 21:55:14 +0100

    Fix type error: for setuptools_scm
    TypeError: setuptools_scm.config.Configuration() got multiple values for keyword argument 'relative_to'
    
    	modified:   pyproject.toml
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,8 @@ from setuptools import find_packages, setup
 with open("requirements.txt") as f:
     REQUIREMENTS = f.read().splitlines()
 
-__FALLBACK_VERSION__ = "0.1"
-
 setup(
     name="astronet",
-    use_scm_version={
-        "root": ".",
-        "relative_to": __file__,
-        "fallback_version": __FALLBACK_VERSION__,
-    },
     setup_requires=["setuptools_scm>=3.5.0"],
     packages=find_packages(),
     install_requires=REQUIREMENTS,
